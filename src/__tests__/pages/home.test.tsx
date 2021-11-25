a833055acb93d346299aa7b40af9a067c3b48098
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-11-25 09:19:15 +0100

    test(pages): add some basic unit tests

---
+++ src/__tests__/pages/home.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from '../../pages/index';
+
+describe('Page: <Home />', () => {
+	beforeEach(() => {
+		render(<Home />);
+	});
+
+	it('Should render a title', () => {
+		const title = screen.getByRole('heading', {
+			name: /welcome to het archief/i,
+		});
+
+		expect(title).toBeInTheDocument();
+	});
+
+	it('Should render a link back to home', () => {
+		const link = screen.getByText(/go to example/i);
+
+		expect(link).toBeInTheDocument();
+	});
+});
