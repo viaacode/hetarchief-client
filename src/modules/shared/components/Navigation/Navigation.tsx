66ac4f9e0f2518a389efc2a0a1570330242a81ae
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-12-07 16:01:30 +0100

    fix(storybook): add sass support

--- src/modules/shared/components/Navigation/Navigation.tsx
+++ src/modules/shared/components/Navigation/Navigation.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 
+import './Navigation.scss';
+
 const Navigation: FC = () => {
-	return <h1>I&apos;m a nav</h1>;
+	return <h1 className="c-navigation">I&apos;m a nav</h1>;
 };
 
 export default Navigation;
