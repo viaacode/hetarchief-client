b14ed38a73facc42e74a27036000ac5f119b2f4f
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-12-08 13:50:08 +0100

    fix(storybook): not able to resolve url() values in scss files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '../src/styles/main.scss';
+import './styleguide.scss';
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
--- src/modules/shared/components/Navigation/Navigation.tsx
+++ src/modules/shared/components/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import styles from './Navigation.module.scss';
 
 const Navigation: FC = () => {
-	return <nav className={styles['c-navigation']} />;
+	return <nav className={styles['c-navigation']}>This is the nav</nav>;
 };
 
 export default Navigation;
