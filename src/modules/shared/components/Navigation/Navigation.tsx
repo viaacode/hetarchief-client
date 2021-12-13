e344402e3a5c507521cc58c17b30f572f7552787
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-12-13 16:40:47 +0100

    feat(navigation): add separate list component and cleanup pages

--- src/modules/shared/components/Navigation/Navigation.tsx
+++ src/modules/shared/components/Navigation/Navigation.tsx
@@ -1,56 +1,24 @@
 import clsx from 'clsx';
-import Link from 'next/link';
 import { FC } from 'react';
 
 import styles from './Navigation.module.scss';
 import {
 	NavigationCenterProps,
 	NavigationFC,
-	NavigationItem,
 	NavigationProps,
 	NavigationSectionProps,
 } from './Navigation.types';
+import NavigationList from './NavigationList/NavigationList';
 
-const renderItems = (items: NavigationItem[][]) => {
-	return items.map((navItems, itemIndex) => {
-		return (
-			<ul key={`nav-list-${itemIndex}`} className={styles['c-navigation__list']}>
-				{navItems.map((item, i) => {
-					const linkCls = clsx(
-						styles['c-navigation__link'],
-						styles[`c-navigation__link--variant-${i + 1}`],
-						{
-							[styles['c-navigation__link--active']]: item.isActive,
-						}
-					);
-
-					return (
-						<li key={item.label} className={styles['c-navigation__item']}>
-							<Link href={item.href}>
-								<a className={linkCls}>
-									{item.label}
-									<span className={styles['c-navigation__border-decoration']} />
-									<span className={styles['c-navigation__border-decoration']} />
-								</a>
-							</Link>
-						</li>
-					);
-				})}
-			</ul>
-		);
-	});
-};
-
-const NavigationLeft: FC<NavigationSectionProps> = ({ children, items }) => (
+const NavigationSection: FC<NavigationSectionProps> = ({ children, items }) => (
 	<div className={styles['c-navigation__section']}>
-		{items && items.length ? renderItems(items) : children}
+		{items && items.length ? <NavigationList items={items} /> : children}
 	</div>
 );
 const NavigationCenter: FC<NavigationCenterProps> = ({ children, title }) => (
-	<>{title ? <h1 className={styles['c-navigation__title']}>{title}</h1> : children}</>
-);
-const NavigationRight: FC<NavigationSectionProps> = ({ children, items }) => (
-	<>{items && items.length ? renderItems(items) : children}</>
+	<div className={styles['c-navigation__section']}>
+		{title ? <h1 className={styles['c-navigation__title']}>{title}</h1> : children}
+	</div>
 );
 
 const Navigation: NavigationFC<NavigationProps> = ({ children, contextual = false }) => {
@@ -61,8 +29,8 @@ const Navigation: NavigationFC<NavigationProps> = ({ children, contextual = fals
 	return <nav className={rootCls}>{children}</nav>;
 };
 
-Navigation.Left = NavigationLeft;
+Navigation.Left = NavigationSection;
 Navigation.Center = NavigationCenter;
-Navigation.Right = NavigationRight;
+Navigation.Right = NavigationSection;
 
 export default Navigation;
