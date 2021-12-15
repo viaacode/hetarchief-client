e90d7bb8becdd5dde3811d6d8a5608d408e9b027
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-12-15 11:27:28 +0100

    feat(footer): make floating action button optional

--- src/modules/shared/components/Footer/Footer.types.ts
+++ src/modules/shared/components/Footer/Footer.types.ts
@@ -2,7 +2,7 @@ export interface FooterProps {
 	links?: FooterLink[];
 	leftItem: FooterItem;
 	rightItem: FooterItem;
-	floatingActionButton: any; // TODO: button component type
+	floatingActionButton?: any; // TODO: button component type
 }
 
 export interface FooterLink {
