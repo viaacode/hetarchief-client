d1c33c9e6ed8709cd3ad21f4e079626301a55726
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-12-16 13:17:56 +0100

    feat: add Card mock data

---
+++ src/modules/shared/components/Card/Card.mock.ts
@@ -0,0 +1,3 @@
+export const title = 'The quick brown fox jumps over the lazy dog';
+export const galaxy =
+	'https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=320&q=50';
