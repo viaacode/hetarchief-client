a4416069c1a31cbae35a0ec0f95f54b363bc7422
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-12-16 13:18:34 +0100

    feat: improve docs and refine horizontal layout

--- src/modules/shared/components/Card/Card.stories.tsx
+++ src/modules/shared/components/Card/Card.stories.tsx
@@ -1,33 +1,37 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Column, Grid, Image } from '@viaa/avo2-components';
 import React from 'react';
+import TruncateMarkup from 'react-truncate-markup';
 
 import Card from './Card';
+import { galaxy, title } from './Card.mock';
 
 export default {
 	title: 'Components/Card',
 	component: Card,
 } as ComponentMeta<typeof Card>;
 
-const title = 'The quick brown fox jumps over the lazy dog';
-const galaxy =
-	'https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=50';
-
 const Template: ComponentStory<typeof Card> = (args) => {
 	return (
 		<>
 			<Grid>
 				<Column size="2">
+					<h4>Simple</h4>
+					<br />
 					<Card {...args} image={undefined} title={undefined}>
 						The simplest card
 					</Card>
 				</Column>
 				<Column size="2">
+					<h4>Border</h4>
+					<br />
 					<Card {...args} title="A card with a zinc-colored border">
 						This card is still pretty basic but also has an image and a title
 					</Card>
 				</Column>
 				<Column size="2">
+					<h4>Edgeless</h4>
+					<br />
 					<Card
 						{...args}
 						edge="none"
@@ -39,6 +43,8 @@ const Template: ComponentStory<typeof Card> = (args) => {
 					</Card>
 				</Column>
 				<Column size="2">
+					<h4>Padded</h4>
+					<br />
 					<Card
 						{...args}
 						padding="both"
@@ -52,6 +58,8 @@ const Template: ComponentStory<typeof Card> = (args) => {
 					</Card>
 				</Column>
 				<Column size="2">
+					<h4>Wide</h4>
+					<br />
 					<Card
 						{...args}
 						padding="content"
@@ -67,6 +75,8 @@ const Template: ComponentStory<typeof Card> = (args) => {
 			<br />
 			<Grid>
 				<Column size="6">
+					<h4>Padded + Horizontal</h4>
+					<br />
 					<Card
 						{...args}
 						padding="both"
@@ -75,27 +85,41 @@ const Template: ComponentStory<typeof Card> = (args) => {
 						subtitle="Studio Hyperdrive (15 dec. 2022) "
 						toolbar="both + horizontal"
 					>
-						Aenean nec feugiat nisi. Pellentesque vel nunc sit amet augue tincidunt
-						egestas. Cras vitae molestie leo. Nullam sed arcu aliquet, porta massa vel,
-						feugiat sapien.
+						<TruncateMarkup lines={2}>
+							<span>
+								Aenean nec feugiat nisi. Pellentesque vel nunc sit amet augue
+								tincidunt egestas. Cras vitae molestie leo. Nullam sed arcu aliquet,
+								porta massa vel, feugiat sapien.
+							</span>
+						</TruncateMarkup>
 					</Card>
 				</Column>
 				<Column size="6">
+					<h4>Wide + Horizontal</h4>
+					<br />
 					<Card
 						{...args}
 						padding="content"
 						orientation="horizontal"
 						title="A horizontal card with padding on just the content"
 						toolbar="content + horizontal"
-					/>
+					>
+						<TruncateMarkup lines={2}>
+							<span>
+								This card is <b>limited</b> to two lines of text in the description,
+								anything that goes beyond that is cut off and replaced with an
+								ellipsis.
+							</span>
+						</TruncateMarkup>
+					</Card>
 				</Column>
 			</Grid>
 		</>
 	);
 };
 
-export const Primary: ComponentStory<typeof Card> = Template.bind({});
-Primary.args = {
+export const Overview: ComponentStory<typeof Card> = Template.bind({});
+Overview.args = {
 	title: <span>{title}</span>,
 	image: <Image wide={true} src={galaxy} alt={title} />,
 	// image: galaxy,
