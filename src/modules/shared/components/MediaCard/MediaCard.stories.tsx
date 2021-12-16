2ad20089d41c56e9ed6805005463ecde81293bdb
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-12-16 13:18:53 +0100

    feat: initial MediaCard component

--- src/modules/shared/components/MediaCard/MediaCard.stories.tsx
+++ src/modules/shared/components/MediaCard/MediaCard.stories.tsx
@@ -1,12 +1,54 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Column, Grid as G } from '@viaa/avo2-components';
 
 import MediaCard from './MediaCard';
+import { soundwave, thumbnail } from './MediaCard.mock';
 
 export default {
 	title: 'Components/MediaCard',
 	component: MediaCard,
 } as ComponentMeta<typeof MediaCard>;
 
-const Template: ComponentStory<typeof MediaCard> = () => <MediaCard />;
+const Template: ComponentStory<typeof MediaCard> = (args) => {
+	return (
+		<G>
+			<Column size={args.view === 'list' ? '12' : '4'}>
+				{args.view === 'list' ? <br /> : undefined}
+				<MediaCard
+					{...args}
+					title="Something you can hear"
+					type="audio"
+					preview={soundwave}
+				/>
+			</Column>
+			<Column size={args.view === 'list' ? '12' : '4'}>
+				{args.view === 'list' ? <br /> : undefined}
+				<MediaCard
+					{...args}
+					title="Something you can watch"
+					type="video"
+					thumbnail={thumbnail}
+				/>
+			</Column>
+			<Column size={args.view === 'list' ? '12' : '4'}>
+				{args.view === 'list' ? <br /> : undefined}
+				<MediaCard {...args} title="Something you know very little about" type="meta" />
+			</Column>
+		</G>
+	);
+};
 
-export const Primary: ComponentStory<typeof MediaCard> = Template.bind({});
+export const Grid: ComponentStory<typeof MediaCard> = Template.bind({});
+Grid.args = {
+	view: 'grid',
+	published_at: new Date(),
+	published_by: 'Studio Hyperdrive',
+	description:
+		'Donec nunc odio, fermentum vel laoreet at, luctus lobortis mauris. Nullam metus lectus, semper vitae nisi in, mollis porttitor nisi. Vivamus pharetra accumsan congue. Aenean commodo leo felis, a placerat velit facilisis non. Proin sollicitudin ultrices mi in viverra. Nam gravida, neque vel porttitor malesuada, velit mi placerat sapien, in fermentum ante neque vitae odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec imperdiet nibh in nisi sagittis, et fermentum turpis dictum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla sollicitudin ipsum eget fermentum iaculis. Nunc facilisis nibh orci, eget euismod lacus suscipit et. Maecenas et tellus consectetur, ornare elit vitae, ullamcorper dolor. Proin vel semper quam. Quisque consequat orci nec semper facilisis. ',
+};
+
+export const List: ComponentStory<typeof MediaCard> = Template.bind({});
+List.args = {
+	...Grid.args,
+	view: 'list',
+};
--- src/modules/shared/components/MediaCard/MediaCard.tsx
+++ src/modules/shared/components/MediaCard/MediaCard.tsx
@@ -1,7 +1,107 @@
-// export interface MediaCardProps {}
-
+import clsx from 'clsx';
 import { FC } from 'react';
+import TruncateMarkup from 'react-truncate-markup';
 
-const MediaCard: FC = () => <>MediaCard</>;
+import { Card, Icon } from '..';
+
+import styles from './MediaCard.module.scss';
+
+import { MediaCardProps } from '.';
+
+const MediaCard: FC<MediaCardProps> = ({
+	description,
+	preview,
+	published_at,
+	published_by,
+	thumbnail,
+	title,
+	type,
+	view,
+}) => {
+	const showDescription = () => {
+		switch (view) {
+			case 'grid':
+				return type === 'video';
+			default:
+				return true;
+		}
+	};
+
+	const renderToolbar = () => (
+		<div className={styles['c-media-card__toolbar']}>
+			{/* TODO: Switch to icon buttons? */}
+			<Icon className={styles['c-media-card__icon']} type="light" name="bookmark" />
+			<Icon className={styles['c-media-card__icon']} type="light" name="dots-vertical" />
+		</div>
+	);
+
+	const renderSubtitle = () => {
+		let subtitle = '';
+
+		if (published_by) {
+			subtitle += published_by;
+		}
+
+		if (published_at) {
+			// TODO: connect to i18n locale
+			const formatted = published_at.toLocaleDateString('nl-BE', {
+				dateStyle: 'medium',
+			});
+
+			subtitle += ` (${formatted})`;
+		}
+
+		return subtitle.length > 0 ? subtitle : undefined;
+	};
+
+	const renderNoVideoIcon = () => (
+		<Icon
+			className={clsx(styles['c-media-card__no-video'], styles['c-media-card__icon'])}
+			type="light"
+			name="no-video"
+		/>
+	);
+
+	const renderHeader = () => {
+		switch (type) {
+			case 'audio':
+				return preview;
+
+			case 'video':
+				return thumbnail;
+
+			default:
+				// 'meta'
+				return view === 'grid' ? (
+					renderNoVideoIcon()
+				) : (
+					<div className={clsx(styles['c-media-card__no-video-wrapper'])}>
+						{renderNoVideoIcon()}
+					</div>
+				);
+		}
+	};
+
+	return (
+		<Card
+			orientation={view === 'grid' ? 'vertical' : 'horizontal'}
+			title={<b>{title}</b>}
+			image={renderHeader()}
+			subtitle={renderSubtitle()}
+			toolbar={renderToolbar()}
+			padding="both"
+		>
+			{/* // Wrapping this in a conditional ensures TruncateMarkup only renders after the content is received */}
+			{description && showDescription() ? (
+				<TruncateMarkup lines={view === 'grid' ? 1 : 2}>
+					<span>{description}</span>
+				</TruncateMarkup>
+			) : (
+				// Passing a child to Card ensure whitespacing at the bottom is applied
+				<></>
+			)}
+		</Card>
+	);
+};
 
 export default MediaCard;
