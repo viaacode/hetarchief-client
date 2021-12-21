8b2aeaff56f17b35f2ee0bd7ff3e57be2817a7ee
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-12-21 11:41:12 +0100

    feat(overlay): add tests

---
+++ src/modules/shared/components/Overlay/Overlay.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react';
+
+import Overlay from './Overlay';
+import styles from './Overlay.module.scss';
+
+fdescribe('Component: <Overlay /> (default)', () => {
+	it('Should render hidden overlay', () => {
+		const testId = 'overlayWrapper';
+
+		render(
+			<div data-testid={testId}>
+				<Overlay />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay']);
+		expect(overlay[0].classList.contains('c-overlay--visible')).toBe(false);
+	});
+
+	it('Should render default visible overlay', () => {
+		const testId = 'overlayWrapper';
+
+		render(
+			<div data-testid={testId}>
+				<Overlay visible />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay']);
+		expect(overlay[0].classList.contains('c-overlay--dark')).toBe(true);
+		expect(overlay[0].classList.contains('c-overlay--visible')).toBe(true);
+	});
+
+	it('Should render dark overlay', () => {
+		const testId = 'overlayWrapper';
+
+		render(
+			<div data-testid={testId}>
+				<Overlay type="dark" visible={true} />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay']);
+		expect(overlay[0].classList.contains('c-overlay--dark')).toBe(true);
+	});
+
+	it('Should render light overlay', () => {
+		const testId = 'overlayWrapper';
+
+		render(
+			<div data-testid={testId}>
+				<Overlay type="light" visible={true} />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay']);
+		expect(overlay[0].classList.contains('c-overlay--light')).toBe(true);
+	});
+});
