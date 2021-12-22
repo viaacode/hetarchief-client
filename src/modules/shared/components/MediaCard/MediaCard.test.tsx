0f235de7c6ab42ef5ec5c71250a675e3169b7e42
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-12-22 18:11:10 +0100

    feat: testing

---
+++ src/modules/shared/components/MediaCard/MediaCard.test.tsx
@@ -0,0 +1,49 @@
+import { render, RenderResult, screen } from '@testing-library/react';
+
+import CardStyles from '../Card/Card.module.scss';
+
+import MediaCard from './MediaCard';
+import { formatDate } from './MediaCard.utils';
+
+import { documentOf } from '@shared/helpers/document-of';
+
+const author = 'Author';
+const now = new Date();
+
+describe('Component: <MediaCard />', () => {
+	let rendered: RenderResult;
+
+	beforeEach(() => {
+		rendered = render(<MediaCard view="grid" published_at={now} published_by={author} />);
+	});
+
+	it('Should apply the vertical orientation when rendered in grid view', () => {
+		const element = documentOf(rendered).getElementsByClassName(
+			CardStyles['c-card--orientation-vertical']
+		);
+
+		expect(element.length).toEqual(1);
+	});
+
+	it('Should apply the horizontal orientation when rendered in list view', () => {
+		rendered = render(<MediaCard view="list" />);
+
+		const element = documentOf(rendered).getElementsByClassName(
+			CardStyles['c-card--orientation-vertical']
+		);
+
+		expect(element.length).toEqual(1);
+	});
+
+	it('Should render the date and author in a specific format', () => {
+		expect(screen.getByText(`${author} (${formatDate(now)})`)).toBeDefined();
+	});
+
+	it('Should show placeholder icons based on the type of card in either view mode', () => {
+		rendered = render(<MediaCard view="list" type="audio" />);
+		expect(screen.getByText('no-audio')).toBeDefined();
+
+		rendered = render(<MediaCard view="grid" type="video" />);
+		expect(screen.getByText('no-video')).toBeDefined();
+	});
+});
