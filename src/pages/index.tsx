fa8bf5f96905b4d75df1f13bfca954f94b5d53b7
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-12-23 13:53:34 +0100

    feat(hero): add hero to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,13 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
 
+import { Hero } from '@shared/components';
+import {
+	heroDescription,
+	heroImage,
+	heroLink,
+	heroTitle,
+} from '@shared/components/Hero/__mocks__/hero';
 import Modal from 'modules/shared/components/Modal/Modal';
 
 const Home: NextPage = () => {
@@ -15,6 +22,12 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
+			<Hero
+				title={heroTitle}
+				description={heroDescription}
+				link={heroLink}
+				image={heroImage}
+			/>
 			<div style={{ display: 'grid', placeItems: 'center', height: '500px' }}>
 				<h1>Welkom in de digitale leeszaal</h1>
 				<Link href="/leeszaal/leeszaal-8">Ga naar leeszaal</Link>
