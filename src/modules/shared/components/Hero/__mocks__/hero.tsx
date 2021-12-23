7282b0071a66882c6e8de8ce2348a54238db5d4c
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-12-23 14:20:14 +0100

    chore(hero): remove double text

--- src/modules/shared/components/Hero/__mocks__/hero.tsx
+++ src/modules/shared/components/Hero/__mocks__/hero.tsx
@@ -1,7 +1,7 @@
 export const heroTitle = 'Welkom in de digitale leeszaal';
 
 export const heroDescription =
-	'Plan een nieuw bezoek, stap fysiek binnen en krijg meteen toegang tot het digitale archief van de leeszaal. Benieuwd hoe het werkt? Hier kom je er alles over te weten.';
+	'Plan een nieuw bezoek, stap fysiek binnen en krijg meteen toegang tot het digitale archief van de leeszaal. Benieuwd hoe het werkt?';
 
 export const heroLink = {
 	label: 'Hier kom je er alles over te weten.',
