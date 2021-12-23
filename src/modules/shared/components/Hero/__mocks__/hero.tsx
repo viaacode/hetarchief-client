960ff14cb468861fe5bea9e17c5365bfc554d753
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-12-23 09:58:21 +0100

    feat(hero): add tests

---
+++ src/modules/shared/components/Hero/__mocks__/hero.tsx
@@ -0,0 +1,11 @@
+export const heroTitle = 'Welkom in de digitale leeszaal';
+
+export const heroDescription =
+	'Plan een nieuw bezoek, stap fysiek binnen en krijg meteen toegang tot het digitale archief van de leeszaal. Benieuwd hoe het werkt? Hier kom je er alles over te weten.';
+
+export const heroLink = {
+	label: 'Hier kom je er alles over te weten.',
+	to: 'https://www.test.com',
+};
+
+export const heroImage = { name: 'hero.png', alt: 'hero image' };
