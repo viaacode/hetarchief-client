4ddbc1449f2d3623c096b242ff0419ede59cdd32
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-04 12:30:59 +0100

    chore(box): implement feedback pr

--- src/modules/shared/components/Box/Box.stories.tsx
+++ src/modules/shared/components/Box/Box.stories.tsx
@@ -2,14 +2,14 @@ import { Box } from '@meemoo/react-components';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import { BoxChildrenMock } from './__mocks__';
+import { boxChildrenMock } from './__mocks__';
 
 export default {
 	title: 'Components/Box',
 	component: Box,
 } as ComponentMeta<typeof Box>;
 
-const Template: ComponentStory<typeof Box> = (args) => <Box {...args}>{BoxChildrenMock}</Box>;
+const Template: ComponentStory<typeof Box> = (args) => <Box {...args}>{boxChildrenMock}</Box>;
 
 export const Default = Template.bind({});
 Default.args = {};
--- src/modules/shared/components/Box/__mocks__/index.tsx
+++ src/modules/shared/components/Box/__mocks__/index.tsx
@@ -1,4 +1,4 @@
-export const BoxChildrenMock = (
+export const boxChildrenMock = (
 	<div style={{ padding: '1rem' }}>
 		<p>Dit is een child van een box component.</p>
 		<p>Deze p-tags zitten wrapped in een div.</p>
