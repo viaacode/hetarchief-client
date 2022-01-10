5cfe4c7a3d5a818c7e9dca1b18d25556a5587509
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-10 16:47:28 +0100

    feat(blade): allow blade interaction in storybook

--- src/modules/shared/components/Blade/Blade.stories.tsx
+++ src/modules/shared/components/Blade/Blade.stories.tsx
@@ -1,22 +1,46 @@
+import { action } from '@storybook/addon-actions';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Blade from './Blade';
+
+import { BladeProps } from '.';
+
+const BladeStoryComponent = ({ args }: { args: BladeProps }) => {
+	const [isOpen, setOpen] = useState(false);
+
+	const open = () => {
+		action('onOpen')();
+		setOpen(true);
+	};
+
+	const close = () => {
+		action('onClose')();
+		setOpen(false);
+	};
+
+	return (
+		<>
+			<div>
+				<h1>Title</h1>
+				<p>Some content</p>
+				<button onClick={open}>open modal</button>
+			</div>
+			<Blade {...args} onClose={close} isOpen={isOpen} />
+		</>
+	);
+};
 
 export default {
 	title: 'Components/Blade',
 	component: Blade,
 } as ComponentMeta<typeof Blade>;
 
-const Template: ComponentStory<typeof Blade> = (args) => (
-	<>
-		<div>
-			<h1>Title</h1>
-			<p>Some content</p>
-		</div>
-		<Blade {...args} />
-	</>
-);
+const Template: ComponentStory<typeof Blade> = (args) => <BladeStoryComponent args={args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+	title: 'title',
+	children: <div>hey</div>,
+	footer: <div>button</div>,
+};
