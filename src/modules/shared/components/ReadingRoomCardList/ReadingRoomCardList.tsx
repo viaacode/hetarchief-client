b3c88a75bd876375d84a1b0fad352c612b8eedca
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-01-11 10:30:29 +0100

    feat: add limiter logic

--- src/modules/shared/components/ReadingRoomCardList/ReadingRoomCardList.tsx
+++ src/modules/shared/components/ReadingRoomCardList/ReadingRoomCardList.tsx
@@ -1,14 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import ReadingRoomCard from '../ReadingRoomCard/ReadingRoomCard';
 
 import styles from './ReadingRoomCardList.module.scss';
 import { ReadingRoomCardListProps } from './ReadingRoomCardList.types';
 
-const ReadingRoomCardList: FC<ReadingRoomCardListProps> = ({ items }) => {
+const defaultProps = {
+	items: [],
+	limit: true,
+};
+
+const ReadingRoomCardList: FC<ReadingRoomCardListProps> = ({ items, limit }) => {
+	const [max, setMax] = useState(3);
+
+	// Only run once to avoid listeners
+	useEffect(() => {
+		if (window.innerWidth >= 768) setMax(6);
+	}, [setMax]);
+
+	const getItems = () => {
+		if (!items) {
+			return [];
+		}
+
+		if (limit) {
+			return items.slice(0, max);
+		}
+
+		return items;
+	};
+
 	return items ? (
 		<ul className={styles['c-reading-room-card-list']}>
-			{items.map((item, i) => (
+			{getItems().map((item, i) => (
 				<li className={styles['c-reading-room-card-list__item']} key={i}>
 					<ReadingRoomCard {...item} />
 				</li>
@@ -16,5 +40,7 @@ const ReadingRoomCardList: FC<ReadingRoomCardListProps> = ({ items }) => {
 		</ul>
 	) : null;
 };
+
+ReadingRoomCardList.defaultProps = defaultProps;
 
 export default ReadingRoomCardList;
--- src/modules/shared/components/ReadingRoomCardList/ReadingRoomCardList.types.tsx
+++ src/modules/shared/components/ReadingRoomCardList/ReadingRoomCardList.types.tsx
@@ -2,4 +2,5 @@ import { ReadingRoomCardProps } from '../ReadingRoomCard/ReadingRoomCard.types';
 
 export interface ReadingRoomCardListProps {
 	items?: ReadingRoomCardProps[];
+	limit?: boolean;
 }
