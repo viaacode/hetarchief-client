2e63689453fb403b1f44fe5e5083e2490ceab9a9
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-11 14:34:34 +0100

    test(overlay): additional tests for new features

--- src/modules/shared/components/Overlay/Overlay.test.tsx
+++ src/modules/shared/components/Overlay/Overlay.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Overlay from './Overlay';
 import styles from './Overlay.module.scss';
@@ -55,5 +55,38 @@ fdescribe('Component: <Overlay /> (default)', () => {
 
 		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay']);
 		expect(overlay[0].classList.contains('c-overlay--light')).toBe(true);
+	});
+
+	it('Should call the onClick handler when clicked', () => {
+		const testId = 'overlayWrapper';
+		const onClickHandler = jest.fn();
+
+		render(
+			<div data-testid={testId}>
+				<Overlay type="light" visible={true} onClick={onClickHandler} />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay'])[0];
+
+		fireEvent.click(overlay);
+
+		expect(onClickHandler).toHaveBeenCalled();
+		expect(onClickHandler).toHaveBeenCalledTimes(1);
+	});
+
+	it('Should set the correct animation class', () => {
+		const testId = 'overlayWrapper';
+		const animate = 'animate-default';
+
+		render(
+			<div data-testid={testId}>
+				<Overlay type="light" visible={true} animate={animate} />
+			</div>
+		);
+
+		const overlay = screen.getByTestId(testId).getElementsByClassName(styles['c-overlay'])[0];
+
+		expect(overlay).toHaveClass(`c-overlay--${animate}`);
 	});
 });
