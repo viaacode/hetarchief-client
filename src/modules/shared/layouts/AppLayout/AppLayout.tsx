bf6d1a367fec76a815a4befa6eeba7630dae67bf
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-01-24 09:23:38 +0100

    feat(arc-158): add auth button in nav and cleanup modal

--- src/modules/shared/layouts/AppLayout/AppLayout.tsx
+++ src/modules/shared/layouts/AppLayout/AppLayout.tsx
@@ -1,17 +1,44 @@
-import { FC } from 'react';
+import { Button } from '@meemoo/react-components';
+import { useTranslation } from 'next-i18next';
+import { FC, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { Footer, Navigation } from '@shared/components';
+import { Footer, Navigation, NavigationItem } from '@shared/components';
 import {
 	footerLeftItem,
 	footerLinks,
 	footerRightItem,
 } from '@shared/components/Footer/__mocks__/footer';
-import {
-	MOCK_ITEMS_LEFT,
-	MOCK_ITEMS_RIGHT,
-} from '@shared/components/Navigation/__mocks__/navigation';
+import { MOCK_ITEMS_LEFT } from '@shared/components/Navigation/__mocks__/navigation';
+import { setShowAuthModal } from '@shared/store/ui';
 
 const AppLayout: FC = ({ children }) => {
+	// TODO: replace with actual logged in state
+	const isLoggedIn = false;
+	const dispatch = useDispatch();
+	const { t } = useTranslation();
+
+	const rightNavItems: NavigationItem[] = useMemo(() => {
+		return isLoggedIn
+			? [
+					// TODO: add notification center and user dropdown menu here
+			  ]
+			: [
+					{
+						id: 'auth-button',
+						node: (
+							<Button
+								label={t(
+									'modules/shared/layouts/app-layout/app-layout___inloggen-of-registreren'
+								)}
+								variants={['white', 'text']}
+								onClick={() => dispatch(setShowAuthModal(true))}
+							/>
+						),
+					},
+			  ];
+	}, [dispatch, isLoggedIn, t]);
+
 	return (
 		<div className="l-app">
 			<Navigation>
@@ -24,7 +51,7 @@ const AppLayout: FC = ({ children }) => {
 						hamburgerLabelClosed: 'Menu',
 					}}
 				/>
-				<Navigation.Right placement="right" items={MOCK_ITEMS_RIGHT} />
+				<Navigation.Right placement="right" items={rightNavItems} />
 			</Navigation>
 
 			<main className="l-app__main">{children}</main>
