61ac3223054db57c65a99756d579cdb178ff2a98
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-01-25 13:03:57 +0100

    feat(arc-165): add requests to logged in hero
    Resolves: ARC-170, ARC-171, ARC-172

--- src/modules/shared/components/Hero/Hero.tsx
+++ src/modules/shared/components/Hero/Hero.tsx
@@ -1,31 +1,113 @@
+import clsx from 'clsx';
+import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
 
+import { ReadingRoomCard, ReadingRoomCardType } from '..';
+
 import styles from './Hero.module.scss';
 import { HeroProps } from './Hero.types';
 
-const Hero: FC<HeroProps> = ({ title, description, link, image }) => {
+const Hero: FC<HeroProps> = ({ title, description, link, image, requests = [], user }) => {
+	const { t } = useTranslation();
+
+	const isLoggedIn = !!user;
+	const rootCls = clsx(styles['c-hero'], { [styles['c-hero--logged-in']]: isLoggedIn });
+
+	const renderNotLoggedIn = () => {
+		return (
+			<>
+				<div className={styles['c-hero__image']}>
+					<Image src={image.src} layout="fill" alt={image.alt} objectFit="contain" />
+				</div>
+				<div className={styles['c-hero__content']}>
+					<h1 className={styles['c-hero__title']}>{title}</h1>
+					<p className={styles['c-hero__description']}>{description}</p>
+					<b>
+						<Link href={link.to}>
+							<a
+								className={styles['c-hero__link']}
+								target={link.external ? '_blank' : '_self'}
+							>
+								{link.label}
+							</a>
+						</Link>
+					</b>
+				</div>
+			</>
+		);
+	};
+
+	const renderLoggedIn = () => {
+		const accessGranted = requests.filter((request) => request.status === 'access');
+		const planned = requests.filter((request) => request.status === 'planned');
+		const requested = requests.filter((request) => request.status === 'requested');
+
+		return (
+			<div className="l-container">
+				<section className="u-flex u-flex-col u-flex-row:md u-justify-between:md u-align-center:md u-mb-48 u-mb-64:md">
+					<h1 className={styles['c-hero__title']}>
+						{t('modules/shared/components/hero/hero___dag-user', {
+							user: user?.firstName,
+						})}
+					</h1>
+					<p className={styles['c-hero__description']}>
+						{t(
+							'Plan een nieuw bezoek, stap fysiek binnen en krijg meteen toegang tot het digitale archief van de leeszaal.'
+						)}
+					</p>
+				</section>
+				{accessGranted.length > 0 && (
+					<section
+						className={clsx(
+							styles['c-hero__section'],
+							styles['c-hero__section--access']
+						)}
+					>
+						{accessGranted.map((room, i) => (
+							<ReadingRoomCard
+								key={`hero-access-granted-${i}`}
+								room={room}
+								type={ReadingRoomCardType['access']}
+							/>
+						))}
+					</section>
+				)}
+				{planned.length > 0 && (
+					<section className={clsx(styles['c-hero__section'])}>
+						<h5 className="u-mb-16">
+							{t('modules/shared/components/hero/hero___geplande-bezoeken')}
+						</h5>
+						{planned.map((room, i) => (
+							<ReadingRoomCard
+								key={`hero-planned-${i}`}
+								room={room}
+								type={ReadingRoomCardType['future--approved']}
+							/>
+						))}
+					</section>
+				)}
+				{requested.length > 0 && (
+					<section className={clsx(styles['c-hero__section'])}>
+						<h5 className="u-mb-16">
+							{t('modules/shared/components/hero/hero___aanvragen')}
+						</h5>
+						{requested.map((room, i) => (
+							<ReadingRoomCard
+								key={`hero-requested-${i}`}
+								room={room}
+								type={ReadingRoomCardType['future--requested']}
+							/>
+						))}
+					</section>
+				)}
+			</div>
+		);
+	};
+
 	return (
-		<header className={styles['c-hero']}>
-			<div className={styles['c-hero__image']}>
-				<Image src={image.src} layout="fill" alt={image.alt} objectFit="contain" />
-			</div>
-			<div className={styles['c-hero__content']}>
-				<h1 className={styles['c-hero__title']}>{title}</h1>
-				<p className={styles['c-hero__description']}>{description}</p>
-				<b>
-					<Link href={link.to}>
-						<a
-							className={styles['c-hero__link']}
-							target={link.external ? '_blank' : '_self'}
-						>
-							{link.label}
-						</a>
-					</Link>
-				</b>
-			</div>
-		</header>
+		<header className={rootCls}>{isLoggedIn ? renderLoggedIn() : renderNotLoggedIn()}</header>
 	);
 };
 
