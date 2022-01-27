53017c8af4b555a0e853de915839e946da27c9a8
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-01-27 15:08:59 +0100

    fix(arc-383): show optional label and add errors

--- src/modules/shared/const/form.ts
+++ src/modules/shared/const/form.ts
@@ -1,3 +1,3 @@
 import { i18n } from 'next-i18next';
 
-export const OPTIONAL_LABEL = i18n?.t('modules/shared/const/form___optioneel');
+export const OPTIONAL_LABEL = () => `(${i18n?.t('modules/shared/const/form___optioneel')})`;
