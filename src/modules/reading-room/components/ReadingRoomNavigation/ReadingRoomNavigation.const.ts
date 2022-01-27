c28cf1881e5cb84517bb6e1e80069a0bd0ccbb57
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-01-27 11:10:16 +0100

    feat(arc-166): cleanup

--- src/modules/reading-room/components/ReadingRoomNavigation/ReadingRoomNavigation.const.ts
+++
@@ -1,4 +0,0 @@
-export const enum READING_ROOM_NAVIGATION__CONTACT_ITEMS {
-	email,
-	phone,
-}
--- src/modules/reading-room/components/ReadingRoomNavigation/index.ts
+++ src/modules/reading-room/components/ReadingRoomNavigation/index.ts
@@ -1,3 +1,2 @@
 export { default as ReadingRoomNavigation } from './ReadingRoomNavigation';
 export * from './ReadingRoomNavigation.types';
-export * from './ReadingRoomNavigation.const';
