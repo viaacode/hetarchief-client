b6c6df480fb5b10857aa9b3cf789973827e287ec
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-02-08 11:12:52 +0100

    fix: use show auth key where possible

--- src/modules/home/utils/query/query.ts
+++ src/modules/home/utils/query/query.ts
@@ -1,11 +1,11 @@
 import { stringify } from 'query-string';
-import { BooleanParam, encodeQueryParams } from 'use-query-params';
+import { encodeQueryParams } from 'use-query-params';
 
-import { SHOW_AUTH_QUERY_KEY } from '@home/const';
+import { HOME_QUERY_PARAM_CONFIG, SHOW_AUTH_QUERY_KEY } from '@home/const';
 
 export const encodeShowAuth = (value: boolean): string => {
 	const encodedShowAuth = encodeQueryParams(
-		{ [SHOW_AUTH_QUERY_KEY]: BooleanParam },
+		{ [SHOW_AUTH_QUERY_KEY]: HOME_QUERY_PARAM_CONFIG[SHOW_AUTH_QUERY_KEY] },
 		{ [SHOW_AUTH_QUERY_KEY]: value }
 	);
 
