220f847f45dd0490adfbfa204bc4575c3bdf2b45
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-02-09 14:50:28 +0100

    feat(arc-440): export sticky layout hook

--- src/modules/shared/hooks/index.ts
+++ src/modules/shared/hooks/index.ts
@@ -2,5 +2,6 @@ export * from './use-blade-manager-context';
 export * from './use-element-size';
 export * from './use-hover';
 export * from './use-scroll-lock';
+export * from './use-sticky-layout';
 export * from './use-window-size';
 export * from './use-window-size-context';
