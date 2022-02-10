e89518eafb9e491d85e94b024d866518a97f23bd
Bert Verhelst <verhelstbert@gmail.com>
2022-02-10 14:30:49 +0100

    fix(docker): disable swc compiler

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WD=/app
-cd $WD
-#echo "setting env"
-#sh ./env.sh
-#rm ./env.sh
-#echo "this is docker entry"
-/app/node_modules/.bin/next start
--- src/modules/shared/helpers/env.ts
+++
@@ -1,3 +0,0 @@
-export function getEnv(name: keyof Window['_ENV_']): string | undefined {
-	return window._ENV_ ? window._ENV_[name] : process.env[name];
-}
