36b1bbc262c011d4b7e49f3e3058776c109c9ce4
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-02-16 16:56:07 +0100

    feat(overlay): account for scrollbar width

--- src/modules/shared/hooks/index.ts
+++ src/modules/shared/hooks/index.ts
@@ -2,6 +2,7 @@ export * from './use-blade-manager-context';
 export * from './use-element-size';
 export * from './use-hover';
 export * from './use-scroll-lock';
+export * from './use-scrollbar-width';
 export * from './use-sticky-layout';
 export * from './use-window-size';
 export * from './use-window-size-context';
--- src/modules/shared/hooks/use-scroll-lock/use-scroll-lock.ts
+++ src/modules/shared/hooks/use-scroll-lock/use-scroll-lock.ts
@@ -1,31 +1,38 @@
 import { useEffect } from 'react';
 
+import useScrollbarWidth from '../use-scrollbar-width/use-scrollbar-width';
+
 import { UseScrollLock } from './use-scroll-lock.types';
 
 const useScrollLock: UseScrollLock = (lock, element) => {
+	const scrollbarWidth = useScrollbarWidth();
+	console.log(scrollbarWidth);
+
 	useEffect(() => {
 		const elementToLock = element ?? document.body;
 		let prevOverflowStyle = '';
-		let prevHeightStyle = '';
+		let prevMarginStyle = '';
 
 		if (!prevOverflowStyle) {
 			prevOverflowStyle = elementToLock.style.overflow;
 		}
-		if (!prevHeightStyle) {
-			prevHeightStyle = elementToLock.style.height;
+		if (!prevMarginStyle) {
+			prevMarginStyle = elementToLock.style.marginRight;
 		}
 
 		if (lock) {
 			elementToLock.style.overflow = 'hidden';
-			elementToLock.style.height = '100vh';
+			elementToLock.style.marginRight = prevMarginStyle
+				? `${prevMarginStyle + scrollbarWidth}px`
+				: `${scrollbarWidth}px`;
 		} else {
 			elementToLock.style.overflow = prevOverflowStyle;
-			elementToLock.style.height = prevHeightStyle;
+			elementToLock.style.marginRight = prevMarginStyle;
 		}
 
 		return () => {
 			elementToLock.style.overflow = prevOverflowStyle;
-			elementToLock.style.height = prevHeightStyle;
+			elementToLock.style.marginRight = prevMarginStyle;
 		};
 	}, [element, lock]);
 };
