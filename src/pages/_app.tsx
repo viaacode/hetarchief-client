cbecc3d7b0d6eed7e2ae9825a49e196913742ac9
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-02-16 15:02:39 +0100

    feat(arc-175): set up SPA-compatible back logic and toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
-import { ReactElement } from 'react';
+import { ReactElement, useCallback, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { AppLayout } from '@shared/layouts/AppLayout';
@@ -8,10 +8,28 @@ import { NextQueryParamProvider } from '@shared/providers/NextQueryParamProvider
 import { wrapper } from '@shared/store';
 
 import 'styles/main.scss';
+import { useRouter } from 'next/router';
+
+import { LOCAL_STORAGE, ROUTES } from '@shared/const';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
+	const router = useRouter();
+
+	useEffect(() => {
+		const blacklist = [ROUTES.termsOfService];
+		const onBeforeHistoryChange = (url: string) => {
+			!blacklist.includes(url) && localStorage.setItem(LOCAL_STORAGE.previousPage, url);
+		};
+
+		router.events.on('beforeHistoryChange', onBeforeHistoryChange);
+
+		return () => {
+			router.events.off('beforeHistoryChange', onBeforeHistoryChange);
+		};
+	});
+
 	return (
 		<NextQueryParamProvider>
 			<QueryClientProvider client={queryClient}>
