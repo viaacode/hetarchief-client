c939bbfb5e6b0a933da3f7f651de67853e6c1385
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-02-20 21:04:30 +0100

    fix(arc-169): align naming of vars

--- src/modules/auth/store/user/user.select.ts
+++ src/modules/auth/store/user/user.select.ts
@@ -4,5 +4,7 @@ import { UserState } from './user.types';
 
 export const selectUser = (state: AppState): UserState['user'] => state.user.user;
 export const selectIsLoggedIn = (state: AppState): boolean => !!state.user.user;
-export const selectHasAcceptedTos = (state: AppState): string | null =>
-	state.user.user?.acceptedTosAt || null;
+
+export const selectHasAcceptedTosAt = (state: AppState): string | null => {
+	return state.user.user?.acceptedTosAt || null;
+};
--- src/modules/shared/hooks/use-terms-of-service/use-terms-of-service.ts
+++ src/modules/shared/hooks/use-terms-of-service/use-terms-of-service.ts
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux';
 
-import { selectHasAcceptedTos } from '@auth/store/user';
+import { selectHasAcceptedTosAt } from '@auth/store/user';
 import { selectTosUpdatedAt } from '@shared/store/tos';
 import { isCurrentTosAccepted } from '@shared/utils';
 
 import { UseTermsOfService } from './use-terms-of-service.types';
 
 const useTermsOfService: UseTermsOfService = () => {
-	const tosAcceptedAt = useSelector(selectHasAcceptedTos);
+	const tosAcceptedAt = useSelector(selectHasAcceptedTosAt);
 	const tosUpdatedAt = useSelector(selectTosUpdatedAt);
 
 	return !!(tosAcceptedAt && tosUpdatedAt && !isCurrentTosAccepted(tosAcceptedAt, tosUpdatedAt));
