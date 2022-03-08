e8028c0ceeed5fa0a7fc26b8b4d47666193ea770
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-03-08 10:24:23 +0100

    feat(arc-349): add rewrites config for admin routes

--- src/modules/admin/index.tsx
+++ src/modules/admin/index.tsx
@@ -6,6 +6,8 @@ const Admin: NextPage = () => {
 	// const [routes] = useModuleRoutes(false);
 
 	return (
+		// The basename prop will ensure all route paths and links are prefixed, otherwise it will
+		// always try to go to the root url
 		<BrowserRouter basename="/admin">
 			<Switch>{/* {routes?.length > 0 && AdminCore.routes.render(routes)} */}</Switch>
 		</BrowserRouter>
