5cabde98bf30fa54e337f155d8ef665612c7aea4
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-03-10 16:21:35 +0100

    chore(arc-570): hide comment

--- src/modules/media/hooks/get-media-info.ts
+++ src/modules/media/hooks/get-media-info.ts
@@ -4,7 +4,6 @@ import { UseQueryResult } from 'react-query/types/react/types';
 import { MediaService } from '@media/services';
 import { Media } from '@media/types';
 import { QUERY_KEYS } from '@shared/const/query-keys';
-// import { ApiResponseWrapper } from '@shared/types/api';
 
 export function useGetMediaInfo(id: string): UseQueryResult<Media> {
 	return useQuery([QUERY_KEYS.getMediaInfo, { id }], () => MediaService.getById(id));
