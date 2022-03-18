f4015df7d758ef7998f4ef1d98363cb5f024ac4b
Bert Verhelst <verhelstbert@gmail.com>
2022-03-18 13:12:59 +0100

    fix(arc-508): simplify format options typing

--- src/modules/shared/helpers/format-belgian-date.ts
+++ src/modules/shared/helpers/format-belgian-date.ts
@@ -1,17 +1,9 @@
 import { format } from 'date-fns';
 
-type DateFnsFormatOptions = {
-	locale?: Locale;
-	weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-	firstWeekContainsDate?: number;
-	useAdditionalWeekYearTokens?: boolean;
-	useAdditionalDayOfYearTokens?: boolean;
-};
-
 export function formatAsBelgianDate(
 	date: string | Date,
 	dateFormat = 'dd/MM/yyyy HH:mm',
-	options: DateFnsFormatOptions = {}
+	options: Parameters<typeof format>[2] = {}
 ): string {
 	let dateObj;
 	if (typeof date === 'string') {
