a326764b6cf832fb8959d2007b2a32d81aa25fbf
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-03-22 11:11:15 +0100

    feat: adjust duration input to required format

--- src/modules/reading-room/components/DurationInput/DurationInput.tsx
+++ src/modules/reading-room/components/DurationInput/DurationInput.tsx
@@ -2,7 +2,7 @@ import { TextInput as Base, TextInputProps } from '@meemoo/react-components';
 import { FC } from 'react';
 
 const DurationInput: FC<TextInputProps> = (props) => (
-	<Base {...props} type="time" pattern="[[0-9]{1,}]*" step="0.001" />
+	<Base {...props} type="time" pattern="([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])" step="1" />
 );
 
 export default DurationInput;
