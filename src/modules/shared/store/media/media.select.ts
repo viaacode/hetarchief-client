cba3fe16bcb6c98b563bd615fc0d3d70f3c663ef
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-03-22 15:43:07 +0100

    chore: remove unused selector

--- src/modules/shared/store/media/media.select.ts
+++ src/modules/shared/store/media/media.select.ts
@@ -1,10 +1,5 @@
-import { MediaSearchAggregation } from '@media/types';
-import { GetMedia, MediaTypes } from '@shared/types';
+import { GetMedia } from '@shared/types';
 
 import { AppState } from '../store.types';
 
 export const selectMediaResults = (state: AppState): GetMedia | undefined => state.media.results;
-export const selectMediaFormatAggregates = (
-	state: AppState
-): MediaSearchAggregation<MediaTypes> | undefined =>
-	state.media.results?.aggregations.dcterms_format;
