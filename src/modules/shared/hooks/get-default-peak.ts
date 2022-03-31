edb93802622b656fca92f852ad98a26dc51bb4dd
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-03-31 13:16:16 +0200

    feat(arc-248): improve typing

--- src/modules/shared/hooks/get-default-peak.ts
+++ src/modules/shared/hooks/get-default-peak.ts
@@ -1,6 +1,7 @@
 import { useQuery, UseQueryResult } from 'react-query';
+import { JsonWaveformData } from 'waveform-data';
 
-export function useGetDefaultPeak(enabled = false): UseQueryResult<any> {
+export function useGetDefaultPeak(enabled = false): UseQueryResult<JsonWaveformData> {
 	return useQuery(
 		['default-peak'],
 		() => fetch('/peak/peakfile.json').then((res) => res.json()),
