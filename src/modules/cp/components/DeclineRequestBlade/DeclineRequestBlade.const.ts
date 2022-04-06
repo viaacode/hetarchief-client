d2990351bd2b433e4a148c57f262d94024d3e39d
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-04-06 15:54:51 +0200

    fix(arc-574): adjust validation

--- src/modules/cp/components/DeclineRequestBlade/DeclineRequestBlade.const.ts
+++ src/modules/cp/components/DeclineRequestBlade/DeclineRequestBlade.const.ts
@@ -1,14 +1,9 @@
-import { i18n } from 'next-i18next';
 import { object, SchemaOf, string } from 'yup';
 
 import { DeclineRequestFormState } from './DeclineRequestBlade.types';
 
 export const DECLINE_REQUEST_FORM_SCHEMA = (): SchemaOf<DeclineRequestFormState> => {
 	return object({
-		reasonForDenial: string().required(
-			i18n?.t(
-				'modules/cp/components/decline-request-blade/decline-request-blade___een-reden-voor-afkeuring-is-verplicht'
-			)
-		),
+		reasonForDenial: string().optional(),
 	});
 };
