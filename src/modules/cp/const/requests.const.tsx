c4dd627409a544004f68ad58d7c209aecae8d659
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-04-26 10:30:10 +0200

    fix(arc-397): set default sorting

--- src/modules/cp/const/requests.const.tsx
+++ src/modules/cp/const/requests.const.tsx
@@ -9,7 +9,7 @@ import { Icon } from '@shared/components';
 import { SEARCH_QUERY_KEY } from '@shared/const';
 import { SortDirectionParam } from '@shared/helpers';
 import { i18n } from '@shared/helpers/i18n';
-import { Visit, VisitRow, VisitStatus } from '@shared/types';
+import { OrderDirection, Visit, VisitRow, VisitStatus } from '@shared/types';
 import { asDate, formatMediumDateWithTime } from '@shared/utils';
 
 export const RequestTablePageSize = 20;
@@ -18,8 +18,8 @@ export const CP_ADMIN_REQUESTS_QUERY_PARAM_CONFIG = {
 	status: withDefault(StringParam, RequestStatusAll.ALL),
 	[SEARCH_QUERY_KEY]: withDefault(StringParam, undefined),
 	page: withDefault(NumberParam, 1),
-	orderProp: withDefault(StringParam, undefined),
-	orderDirection: withDefault(SortDirectionParam, undefined),
+	orderProp: withDefault(StringParam, 'createdAt'),
+	orderDirection: withDefault(SortDirectionParam, OrderDirection.desc),
 };
 
 export const requestStatusFilters = (): TabProps[] => {
