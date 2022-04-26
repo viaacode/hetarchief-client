b836805f1768be330bc9b004d3ff99b6c4f78b20
Bert Verhelst <verhelstbert@gmail.com>
2022-04-26 17:51:27 +0200

    feat(arc-161): Load cookiebot script using Script from nextjs

--- src/pages/cookiebeleid/index.tsx
+++ src/pages/cookiebeleid/index.tsx
@@ -1,28 +1,19 @@
 import { NextPage } from 'next';
-import React, { createRef, useEffect } from 'react';
+import Script from 'next/script';
+import React from 'react';
 
 import styles from './cookie-policy.module.scss';
 
 const CookiePolicy: NextPage = () => {
-	const cookieBotWrapper = createRef<HTMLDivElement>();
-
-	useEffect(() => {
-		if (cookieBotWrapper.current) {
-			const script = document.createElement('script');
-			script.setAttribute('id', 'CookieDeclaration');
-			script.src = 'https://consent.cookiebot.com/8fb68e92-94b2-4334-bc47-7bcda08bc9c7/cd.js';
-			cookieBotWrapper.current.innerHTML = '';
-			cookieBotWrapper.current.append(script);
-		}
-
-		return () => {
-			if (cookieBotWrapper.current) {
-				cookieBotWrapper.current.innerHTML = '';
-			}
-		};
-	}, [cookieBotWrapper]);
-
-	return <div className={styles['cookie-policy__wrapper']} ref={cookieBotWrapper} />;
+	return (
+		<div className={styles['cookie-policy__wrapper']}>
+			<Script
+				src="https://consent.cookiebot.com/8fb68e92-94b2-4334-bc47-7bcda08bc9c7/cd.js"
+				id="CookieDeclaration"
+				strategy="beforeInteractive"
+			/>
+		</div>
+	);
 };
 
 export default CookiePolicy;
