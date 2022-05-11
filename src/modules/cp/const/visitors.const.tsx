63cedd624526a4605c38d1afdef5c3cfbf29002f
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-05-11 15:22:52 +0200

    fix: avoid applying height where not needed

--- src/modules/cp/const/visitors.const.tsx
+++ src/modules/cp/const/visitors.const.tsx
@@ -52,7 +52,7 @@ export const VisitorsTableColumns = (
 			<CopyButton
 				className="u-color-neutral u-p-0 c-table__copy"
 				icon={undefined}
-				variants={['text', 'no-height']}
+				variants="text"
 				text={row.original.visitorMail}
 			>
 				{row.original.visitorMail}
