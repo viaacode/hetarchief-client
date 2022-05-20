697bd49258a8293016cb4a4a82e600e1206882a1
Bert Verhelst <verhelstbert@gmail.com>
2022-05-20 16:52:20 +0200

    feat(arc-610): add content pages

--- src/modules/admin/layouts/AdminLayout/AdminLayout.types.ts
+++ src/modules/admin/layouts/AdminLayout/AdminLayout.types.ts
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react';
 import { DefaultComponentProps } from '@shared/types';
 
 export interface AdminLayoutProps extends DefaultComponentProps {
-	pageTitle: ReactNode;
+	pageTitle?: ReactNode;
 }
 
 export type AdminLayoutComponent = FC<AdminLayoutProps> & {
