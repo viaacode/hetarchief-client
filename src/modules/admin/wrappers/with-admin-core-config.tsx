63d147f3b317e7d87d10b37760a54cfd03707525
Bert Verhelst <verhelstbert@gmail.com>
2022-05-23 10:17:14 +0200

    fix(arc-611): fix router functions in admin core config object

--- src/modules/admin/wrappers/with-admin-core-config.tsx
+++ src/modules/admin/wrappers/with-admin-core-config.tsx
@@ -10,6 +10,7 @@ import { CommonUser } from '@meemoo/react-admin/dist/cjs/modules/user/user.types
 import { i18n } from 'next-i18next';
 import getConfig from 'next/config';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 // import { useRouter } from 'next/router';
 import { ComponentType, FunctionComponent, useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -38,29 +39,14 @@ export const withAdminCoreConfig = (WrappedComponent: ComponentType): ComponentT
 		const [adminCoreConfig, setAdminCoreConfig] = useState<ConfigValue | null>(null);
 		// eslint-disable-next-line react-hooks/rules-of-hooks
 		const user = useSelector(selectUser);
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const router = useRouter();
 
 		// eslint-disable-next-line react-hooks/rules-of-hooks
 		const initConfigValue = useCallback(() => {
 			if (!i18n) {
 				return;
 			}
-
-			const routerConfig: ConfigValue['services']['router'] = {
-				Link: InternalLink as FunctionComponent<LinkInfo>,
-				useHistory: () => ({
-					push: () => {
-						// empty
-					},
-					replace: () => {
-						// empty
-					},
-				}), //useRouter,
-				useParams: () => {
-					// const router = useRouter();
-					// return router.query as Record<string, string>;
-					return {};
-				},
-			};
 
 			const commonUser: CommonUser = {
 				uid: user?.id,
@@ -132,7 +118,16 @@ export const withAdminCoreConfig = (WrappedComponent: ComponentType): ComponentT
 						fetchCities: () => Promise.resolve([]),
 						fetchEducationOrganisations: () => Promise.resolve([]),
 					},
-					router: routerConfig as any,
+					router: {
+						Link: InternalLink as FunctionComponent<LinkInfo>,
+						useHistory: () => ({
+							push: router.push,
+							replace: router.replace,
+						}), //useRouter,
+						useParams: () => {
+							return router.query as Record<string, string>;
+						},
+					},
 					queryCache: {
 						// eslint-disable-next-line @typescript-eslint/no-unused-vars
 						clear: async (_key: string) => Promise.resolve(),
