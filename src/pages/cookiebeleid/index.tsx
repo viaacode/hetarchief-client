7845213383ce19aa3eefc590d75d5396712e8517
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-06-16 16:52:17 +0200

    fix(arc-161): load cookiebot a different way

--- src/pages/cookiebeleid/index.tsx
+++ src/pages/cookiebeleid/index.tsx
@@ -8,25 +8,18 @@ import styles from './cookie-policy.module.scss';
 const CookiePolicy: NextPage = () => {
 	const [cookieDeclarationHtml, setCookieDeclarationHtml] = useState<string>('');
 
-	const moveCookieDeclaration = () => {
-		const declaration = document.getElementsByClassName('CookieDeclaration');
-		if (
-			!declaration[0] ||
-			!declaration?.[0]?.parentElement?.innerHTML?.toLowerCase()?.includes('meemoo')
-		) {
-			setTimeout(moveCookieDeclaration, 100);
-			return;
-		}
-		const parent = declaration[0].parentElement;
-		setCookieDeclarationHtml(parent?.innerHTML || '');
-		parent?.remove();
-	};
-
 	useEffect(() => {
+		// Fool cookiebot to inject the html into our react useState
+		(window as any).CookieDeclaration = {
+			InjectCookieDeclaration: setCookieDeclarationHtml,
+		};
+
+		// Load cookiebot script that has the cookie declaration html baked in
 		const script = document.createElement('script');
-		script.onload = moveCookieDeclaration;
+		// script.onload = moveCookieDeclaration;
 		script.id = 'CookieDeclaration';
-		script.src = 'https://consent.cookiebot.com/e17bca33-78a0-484e-a204-e05274a65598/cd.js';
+		script.src =
+			'https://consent.cookiebot.com/e17bca33-78a0-484e-a204-e05274a65598/cdreport.js';
 		document.head.appendChild(script);
 	}, []);
 
