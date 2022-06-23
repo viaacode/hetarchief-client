02e900aae16c075753452ce1a9134ca132d64215
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-06-23 16:18:00 +0200

    fix(arc-1043): Mark single unmanaged open blade with .c-blade--active

--- src/modules/shared/components/Blade/Blade.tsx
+++ src/modules/shared/components/Blade/Blade.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@meemoo/react-components';
 import clsx from 'clsx';
 import FocusTrap from 'focus-trap-react';
+import { isUndefined } from 'lodash-es';
 import { FC } from 'react';
 
 import { useBladeManagerContext } from '@shared/hooks/use-blade-manager-context';
@@ -61,7 +62,9 @@ const Blade: FC<BladeProps> = ({
 					className,
 					styles['c-blade'],
 					isBladeOpen && styles['c-blade--visible'],
-					layer === currentLayer && 'c-blade--active',
+					isBladeOpen &&
+						(layer === currentLayer || (currentLayer === 0 && isUndefined(layer))) &&
+						'c-blade--active',
 					isLayered && [styles['c-blade--managed']]
 				)}
 				// offset underlying blades
