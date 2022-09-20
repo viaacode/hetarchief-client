1e3d315f67689665d45f6b8e96a94e2e2ea1abbe
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-09-20 16:29:47 +0200

    fix(arc-395): only render blade content when open

--- src/modules/shared/components/DeclineRequestBlade/DeclineRequestBlade.tsx
+++ src/modules/shared/components/DeclineRequestBlade/DeclineRequestBlade.tsx
@@ -91,25 +91,28 @@ const DeclineRequestBlade: FC<DeclineRequestBladeProps> = (props) => {
 			)}
 		>
 			{selected && <VisitSummary {...selected} />}
-			<div className="u-px-32">
-				<FormControl
-					className="u-mb-24"
-					errors={[errors.reasonForDenial?.message]}
-					id={labelKeys.reasonForDenial}
-					label={t(
-						'modules/cp/components/decline-request-blade/decline-request-blade___reden-voor-afkeuring'
-					)}
-					suffix={OPTIONAL_LABEL()}
-				>
-					<Controller
-						name="reasonForDenial"
-						control={control}
-						render={({ field }) => (
-							<TextArea {...field} id={labelKeys.reasonForDenial} />
+
+			{props.isOpen && (
+				<div className="u-px-32">
+					<FormControl
+						className="u-mb-24"
+						errors={[errors.reasonForDenial?.message]}
+						id={labelKeys.reasonForDenial}
+						label={t(
+							'modules/cp/components/decline-request-blade/decline-request-blade___reden-voor-afkeuring'
 						)}
-					/>
-				</FormControl>
-			</div>
+						suffix={OPTIONAL_LABEL()}
+					>
+						<Controller
+							name="reasonForDenial"
+							control={control}
+							render={({ field }) => (
+								<TextArea {...field} id={labelKeys.reasonForDenial} />
+							)}
+						/>
+					</FormControl>
+				</div>
+			)}
 		</Blade>
 	);
 };
