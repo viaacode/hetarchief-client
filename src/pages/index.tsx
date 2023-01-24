d1e7394d6f02f3ebc1fe4933a9903fa8a5da5055
Ward Vercruyssen <wardverc@Wards-MacBook-Pro.local>
2023-01-24 10:55:40 +0100

    feat(ARC-1251): remove visitorspace logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,32 @@
 import { ContentPageRenderer } from '@meemoo/admin-core-ui';
-import { HTTPError } from 'ky';
 import { GetServerSidePropsResult, NextPage } from 'next';
-import getConfig from 'next/config';
 import { useRouter } from 'next/router';
 import { GetServerSidePropsContext } from 'next/types';
-import { ComponentType, useEffect } from 'react';
+import { ComponentType } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BooleanParam, StringParam, useQueryParams, withDefault } from 'use-query-params';
+import { BooleanParam, useQueryParams } from 'use-query-params';
 
 import { withAdminCoreConfig } from '@admin/wrappers/with-admin-core-config';
 import { AuthModal } from '@auth/components';
 import { selectUser } from '@auth/store/user';
-import { SHOW_AUTH_QUERY_KEY, VISITOR_SPACE_SLUG_QUERY_KEY } from '@home/const';
+import { SHOW_AUTH_QUERY_KEY } from '@home/const';
 import { Loading } from '@shared/components';
 import { getDefaultServerSideProps } from '@shared/helpers/get-default-server-side-props';
 import { renderOgTags } from '@shared/helpers/render-og-tags';
 import { useNavigationBorder } from '@shared/hooks/use-navigation-border';
-import { selectShowAuthModal, setShowAuthModal, setShowZendesk } from '@shared/store/ui';
+import { selectShowAuthModal, setShowAuthModal } from '@shared/store/ui';
 import { DefaultSeoInfo } from '@shared/types/seo';
-import VisitorSpaceSearchPage from '@visitor-space/components/VisitorSpaceSearchPage/VisitorSpaceSearchPage';
-import { useGetVisitorSpace } from '@visitor-space/hooks/get-visitor-space';
-import { VisitorSpaceService } from '@visitor-space/services';
 
 import { useGetContentPage } from '../modules/content-page/hooks/get-content-page';
 import { ContentPageService } from '../modules/content-page/services/content-page.service';
 
 import { VisitorLayout } from 'modules/visitors';
 
-const { publicRuntimeConfig } = getConfig();
-
-type DynamicRouteResolverProps = {
+type HomepageProps = {
 	title: string | null;
 } & DefaultSeoInfo;
 
-const DynamicRouteResolver: NextPage<DynamicRouteResolverProps> = ({ title, url }) => {
+const Homepage: NextPage<HomepageProps> = ({ title, url }) => {
 	useNavigationBorder();
 
 	const router = useRouter();
@@ -43,32 +36,16 @@ const DynamicRouteResolver: NextPage<DynamicRouteResolverProps> = ({ title, url 
 	const showAuthModal = useSelector(selectShowAuthModal);
 	const [query, setQuery] = useQueryParams({
 		[SHOW_AUTH_QUERY_KEY]: BooleanParam,
-		[VISITOR_SPACE_SLUG_QUERY_KEY]: withDefault(StringParam, undefined),
 	});
 
 	/**
 	 * Data
 	 */
 
-	const {
-		error: visitorSpaceError,
-		isLoading: isVisitorSpaceLoading,
-		data: visitorSpaceInfo,
-	} = useGetVisitorSpace(slug as string, true);
-	const {
-		error: contentPageError,
-		isLoading: isContentPageLoading,
-		data: contentPageInfo,
-	} = useGetContentPage(slug as string, true);
-
-	/**
-	 * Computed
-	 */
-
-	const isVisitorSpaceNotFoundError = (visitorSpaceError as HTTPError)?.response?.status === 404;
-	const isContentPageNotFoundError =
-		(!!contentPageInfo && contentPageInfo?.exists === false) ||
-		(contentPageError as HTTPError)?.response?.status === 404;
+	const { isLoading: isContentPageLoading, data: contentPageInfo } = useGetContentPage(
+		slug as string,
+		true
+	);
 
 	/**
 	 * Methods
@@ -78,39 +55,18 @@ const DynamicRouteResolver: NextPage<DynamicRouteResolverProps> = ({ title, url 
 		if (typeof query[SHOW_AUTH_QUERY_KEY] === 'boolean') {
 			setQuery({
 				[SHOW_AUTH_QUERY_KEY]: undefined,
-				[VISITOR_SPACE_SLUG_QUERY_KEY]: undefined,
 			});
 		}
 		dispatch(setShowAuthModal(false));
 	};
 
 	/**
-	 * Effects
-	 */
-
-	useEffect(() => {
-		if (isVisitorSpaceNotFoundError && isContentPageNotFoundError) {
-			window.open(`${publicRuntimeConfig.PROXY_URL}/not-found`, '_self');
-		}
-	}, [isVisitorSpaceNotFoundError, isContentPageNotFoundError]);
-
-	useEffect(() => {
-		dispatch(setShowZendesk(true));
-	}, [dispatch]);
-
-	/**
 	 * Render
 	 */
 
 	const renderPageContent = () => {
-		dispatch(setShowZendesk(true));
-
-		if (isVisitorSpaceLoading || isContentPageLoading) {
+		if (isContentPageLoading) {
 			return <Loading fullscreen owner="slug page: render page content" />;
-		}
-		if (visitorSpaceInfo) {
-			dispatch(setShowZendesk(false));
-			return <VisitorSpaceSearchPage />;
 		}
 		if (contentPageInfo) {
 			return <ContentPageRenderer path={'/' as string} userGroupId={user?.groupId} />;
@@ -128,24 +84,18 @@ const DynamicRouteResolver: NextPage<DynamicRouteResolverProps> = ({ title, url 
 
 export async function getServerSideProps(
 	context: GetServerSidePropsContext
-): Promise<GetServerSidePropsResult<DynamicRouteResolverProps>> {
+): Promise<GetServerSidePropsResult<HomepageProps>> {
 	let title: string | null = null;
 	try {
-		const [space, contentPage] = await Promise.allSettled([
-			VisitorSpaceService.getBySlug(context.query.slug as string, true),
+		const [contentPage] = await Promise.allSettled([
 			ContentPageService.getBySlug(('/' + context.query.slug) as string),
 		]);
 
-		if (space.status === 'fulfilled') {
-			title = space.value?.name || null;
-		} else if (contentPage.status === 'fulfilled') {
+		if (contentPage.status === 'fulfilled') {
 			title = contentPage.value?.title || null;
 		}
 	} catch (err) {
-		console.error(
-			'Failed to fetch visitor space or content page seo info by slug: ' + context.query.slug,
-			err
-		);
+		console.error('Failed to fetch content page seo info by slug: ' + context.query.slug, err);
 	}
 
 	const defaultProps: GetServerSidePropsResult<DefaultSeoInfo> = await getDefaultServerSideProps(
@@ -157,4 +107,4 @@ export async function getServerSideProps(
 	};
 }
 
-export default withAdminCoreConfig(DynamicRouteResolver as ComponentType);
+export default withAdminCoreConfig(Homepage as ComponentType);
