5f67a33a198ad31e1d784c7e3ae19cd4c01afa45
Ward Vercruyssen <wardverc@Wards-MacBook-Pro.local>
2023-01-25 09:42:30 +0100

    feat(ARC-1226): fix overflow with css

--- src/modules/visitor-space/components/FilterMenu/FilterMenu.tsx
+++ src/modules/visitor-space/components/FilterMenu/FilterMenu.tsx
@@ -96,12 +96,9 @@ const FilterMenu: FC<FilterMenuProps> = ({
 	 */
 
 	const renderActiveSortLabel = () => {
-		let sortBtnLabel = activeSort
+		const sortBtnLabel = activeSort
 			? VISITOR_SPACE_ACTIVE_SORT_MAP()[activeSort.orderProp as VisitorSpaceSort]
 			: '';
-		if (!isMobile && sortBtnLabel.length > 15) {
-			sortBtnLabel = sortBtnLabel.substring(0, 15) + '...';
-		}
 
 		return (
 			<>
