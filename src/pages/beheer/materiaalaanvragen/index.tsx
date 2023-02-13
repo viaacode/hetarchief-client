72c58fa5d6cd1860633413d382b4f6d52a5296ee
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-02-13 14:03:19 +0100

    feat(materiaalaanvragen): add new page

---
+++ src/pages/beheer/materiaalaanvragen/index.tsx
@@ -0,0 +1,50 @@
+import { GetServerSidePropsResult, NextPage } from 'next';
+import { GetServerSidePropsContext } from 'next/types';
+import { ComponentType } from 'react';
+
+import { Permission } from '@account/const';
+import { withAuth } from '@auth/wrappers/with-auth';
+import { CPAdminLayout } from '@cp/layouts';
+import PermissionsCheck from '@shared/components/PermissionsCheck/PermissionsCheck';
+import { getDefaultServerSideProps } from '@shared/helpers/get-default-server-side-props';
+import { renderOgTags } from '@shared/helpers/render-og-tags';
+import useTranslation from '@shared/hooks/use-translation/use-translation';
+import { DefaultSeoInfo } from '@shared/types/seo';
+
+const CPMaterialRequestsPage: NextPage<DefaultSeoInfo> = ({ url }) => {
+	const { tText } = useTranslation();
+
+	const renderPageContent = () => {
+		return (
+			<CPAdminLayout
+				className="p-cp-requests"
+				pageTitle={tText('pages/beheer/materiaalaanvragen/index___materiaalaanvragen')}
+			>
+				<h1>REQUEST</h1>
+			</CPAdminLayout>
+		);
+	};
+
+	return (
+		<>
+			{renderOgTags(
+				tText('pages/beheer/materiaalaanvragen/index___materiaalaanvragen'),
+				tText(
+					'pages/beheer/materiaalaanvragen/index___materiaalaanvragen-meta-omschrijving'
+				),
+				url
+			)}
+
+			{/* TODO: check permissie */}
+			<PermissionsCheck allPermissions={[]}>{renderPageContent()}</PermissionsCheck>
+		</>
+	);
+};
+
+export async function getServerSideProps(
+	context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<DefaultSeoInfo>> {
+	return getDefaultServerSideProps(context);
+}
+
+export default withAuth(CPMaterialRequestsPage as ComponentType);
