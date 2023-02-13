dd1c3dbb02fb66164c708ef06339770cd6f9e0bd
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-02-13 18:50:14 +0100

    feat(Icon): do not render non existing icons + fix test

--- src/modules/shared/components/Icon/Icon.test.tsx
+++ src/modules/shared/components/Icon/Icon.test.tsx
@@ -3,13 +3,15 @@ import { render, screen } from '@testing-library/react';
 import Icon from './Icon';
 import { IconNamesLight, IconNamesSolid } from './Icon.const';
 
+const parseIconName = (name: IconNamesLight | IconNamesSolid): string => name.split('--')[0];
+
 describe('Components', () => {
 	describe('<Icon />', () => {
 		it('Should display light icons', () => {
 			const iconName = IconNamesSolid.Play;
 			render(<Icon name={iconName} />);
 
-			const icon = screen.queryByText(iconName);
+			const icon = screen.queryByText(parseIconName(iconName));
 			expect(icon).toBeInTheDocument();
 		});
 
@@ -17,7 +19,7 @@ describe('Components', () => {
 			const iconName = IconNamesSolid.Trash;
 			render(<Icon name={iconName} />);
 
-			const icon = screen.queryByText(iconName);
+			const icon = screen.queryByText(parseIconName(iconName));
 			expect(icon).toHaveClass('c-icon--solid');
 		});
 
@@ -34,7 +36,7 @@ describe('Components', () => {
 			const iconName = IconNamesLight.Bookmark;
 			render(<Icon className={customClass} name={iconName} />);
 
-			const icon = screen.queryByText(iconName);
+			const icon = screen.queryByText(parseIconName(iconName));
 			expect(icon).toHaveClass(customClass);
 		});
 	});
