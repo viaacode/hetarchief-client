54382e229e3270db42bfe4018052367fd788f3d1
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-02-13 16:27:59 +0100

    feat(cp): add table config for material requests

---
+++ src/modules/cp/const/material-requests.const.tsx
@@ -0,0 +1,50 @@
+import { Column } from 'react-table';
+
+import { CopyButton } from '@shared/components';
+import { tText } from '@shared/helpers/translate';
+import { asDate, formatDistanceToday, formatMediumDateWithTime } from '@shared/utils';
+
+import { MaterialRequest, MaterialRequestRow } from '@material-requests/types';
+
+export const MaterialRequestTableColumns = (): Column<MaterialRequest>[] => [
+	{
+		Header: tText('modules/cp/const/material-requests___naam'),
+		accessor: 'requesterName',
+	},
+	{
+		Header: tText('modules/cp/const/material-requests___emailadres'),
+		accessor: 'requesterEmail',
+		Cell: ({ row }: MaterialRequestRow) => (
+			<CopyButton
+				className="u-color-neutral u-p-0 c-table__copy"
+				icon={undefined}
+				variants="text"
+				text={row.original.requesterEmail}
+			>
+				{row.original.requesterEmail}
+			</CopyButton>
+		),
+	},
+	{
+		Header: tText('modules/cp/const/material-requests___tijdstip'),
+		accessor: 'createdAt',
+		Cell: ({ row }: MaterialRequestRow) => (
+			<span
+				className="u-color-neutral"
+				title={formatMediumDateWithTime(asDate(row.original.createdAt))}
+			>
+				{formatDistanceToday(row.original.createdAt)}
+			</span>
+		),
+	},
+	{
+		Header: tText('modules/cp/const/material-requests___type'),
+		accessor: 'type',
+		Cell: ({ row }: MaterialRequestRow) => (
+			<span className="u-color-neutral">
+				{/* TODO TRANSLATIONS FOR TYPES */}
+				{row.original.type}
+			</span>
+		),
+	},
+];
