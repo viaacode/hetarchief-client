cc5505c779d61195741c7a1f0906c9b9269f022d
Ward Vercruyssen <wardverc@Wards-MacBook-Pro.local>
2023-02-20 16:01:05 +0100

    feat(ARC-1247): Added admin material request overview table with search

---
+++ src/modules/admin/const/material-requests.const.tsx
@@ -0,0 +1,63 @@
+import { Column } from 'react-table';
+import { NumberParam, StringParam, withDefault } from 'use-query-params';
+
+import { SortDirectionParam } from '@shared/helpers';
+import { tText } from '@shared/helpers/translate';
+import { asDate, formatDistanceToday, formatMediumDateWithTime } from '@shared/utils';
+
+import { MATERIAL_REQUEST_TRANSLATIONS_BY_TYPE } from '@material-requests/const';
+import { MaterialRequest, MaterialRequestKeys, MaterialRequestRow } from '@material-requests/types';
+
+export const ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE = 20;
+
+export const ADMIN_MATERIAL_REQUESTS_QUERY_PARAM_CONFIG = {
+	orderProp: withDefault(StringParam, MaterialRequestKeys.createdAt),
+	orderDirection: withDefault(SortDirectionParam, undefined),
+	page: withDefault(NumberParam, 1),
+	search: withDefault(StringParam, ''),
+};
+
+export const getAdminMaterialRequestTableColumns = (): Column<MaterialRequest>[] => [
+	{
+		Header: tText('modules/admin/const/material-requests___naam'),
+		accessor: MaterialRequestKeys.name,
+		Cell: ({ row: { original } }: MaterialRequestRow) => (
+			<span className="u-color-neutral">{original.requesterFullName}</span>
+		),
+	},
+	{
+		Header: tText('modules/admin/const/material-requests___emailadres'),
+		accessor: MaterialRequestKeys.email,
+		Cell: ({ row: { original } }: MaterialRequestRow) => (
+			<span className="u-color-neutral">{original.requesterMail}</span>
+		),
+	},
+	{
+		Header: tText('modules/admin/const/material-requests___aanbieder'),
+		accessor: MaterialRequestKeys.maintainer,
+		Cell: ({ row: { original } }: MaterialRequestRow) => (
+			<span className="u-color-neutral">{original.maintainerName}</span>
+		),
+	},
+	{
+		Header: tText('modules/admin/const/material-requests___datum'),
+		accessor: MaterialRequestKeys.createdAt,
+		Cell: ({ row: { original } }: MaterialRequestRow) => (
+			<span
+				className="u-color-neutral"
+				title={formatMediumDateWithTime(asDate(original.createdAt))}
+			>
+				{formatDistanceToday(original.createdAt)}
+			</span>
+		),
+	},
+	{
+		Header: tText('modules/admin/const/material-requests___type'),
+		accessor: MaterialRequestKeys.type,
+		Cell: ({ row: { original } }: MaterialRequestRow) => (
+			<span className="u-color-neutral p-material-requests__table-type">
+				{MATERIAL_REQUEST_TRANSLATIONS_BY_TYPE[original.type]}
+			</span>
+		),
+	},
+];
--- src/pages/admin/materiaalaanvragen/index.tsx
+++ src/pages/admin/materiaalaanvragen/index.tsx
@@ -1,25 +1,136 @@
+import { OrderDirection, Table } from '@meemoo/react-components';
+import clsx from 'clsx';
+import { isEmpty, isNil } from 'lodash';
 import { GetServerSidePropsResult, NextPage } from 'next';
 import { GetServerSidePropsContext } from 'next/types';
-import React, { ComponentType } from 'react';
+import React, { ComponentType, ReactNode, useMemo } from 'react';
+import { SortingRule, TableState } from 'react-table';
+import { useQueryParams } from 'use-query-params';
 
 import { Permission } from '@account/const';
+import {
+	ADMIN_MATERIAL_REQUESTS_QUERY_PARAM_CONFIG,
+	ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE,
+	getAdminMaterialRequestTableColumns,
+} from '@admin/const/material-requests.const';
 import { AdminLayout } from '@admin/layouts';
 import { withAuth } from '@auth/wrappers/with-auth';
+import { Loading, PaginationBar, SearchBar, sortingIcons } from '@shared/components';
 import PermissionsCheck from '@shared/components/PermissionsCheck/PermissionsCheck';
+import { globalLabelKeys } from '@shared/const';
 import { getDefaultServerSideProps } from '@shared/helpers/get-default-server-side-props';
 import { renderOgTags } from '@shared/helpers/render-og-tags';
 import useTranslation from '@shared/hooks/use-translation/use-translation';
 import { DefaultSeoInfo } from '@shared/types/seo';
 
+import { useGetMaterialRequests } from '@material-requests/hooks/get-material-requests';
+import { MaterialRequest, MaterialRequestKeys } from '@material-requests/types';
+
 const AdminMaterialRequests: NextPage<DefaultSeoInfo> = ({ url }) => {
-	const { tText } = useTranslation();
+	const { tHtml, tText } = useTranslation();
+	const [filters, setFilters] = useQueryParams(ADMIN_MATERIAL_REQUESTS_QUERY_PARAM_CONFIG);
+
+	const { data: materialRequests, isFetching } = useGetMaterialRequests({
+		isPersonal: false,
+		size: ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE,
+		...(!isNil(filters.page) && { page: filters.page }),
+		...(!isNil(filters.orderProp) && { orderProp: filters.orderProp as MaterialRequestKeys }),
+		...(!isNil(filters.orderDirection) && {
+			orderDirection: filters.orderDirection as OrderDirection,
+		}),
+		search: filters.search,
+	});
+
+	const noData = useMemo((): boolean => isEmpty(materialRequests?.items), [materialRequests]);
+
+	const sortFilters = useMemo(
+		(): SortingRule<{ id: MaterialRequestKeys; desc: boolean }>[] => [
+			{
+				id: filters.orderProp,
+				desc: filters.orderDirection !== OrderDirection.asc,
+			},
+		],
+		[filters]
+	);
+
+	const onSortChange = (
+		orderProp: string | undefined,
+		orderDirection: OrderDirection | undefined
+	): void => {
+		if (filters.orderProp !== orderProp || filters.orderDirection !== orderDirection) {
+			setFilters({
+				...filters,
+				orderProp,
+				orderDirection,
+				page: 1,
+			});
+		}
+	};
+
+	const onPageChange = (pageZeroBased: number, gotoPage: (i: number) => void): void => {
+		gotoPage(pageZeroBased);
+		setFilters({
+			...filters,
+			page: pageZeroBased + 1,
+		});
+	};
+
+	const renderPagination = ({ gotoPage }: { gotoPage: (i: number) => void }): ReactNode => (
+		<PaginationBar
+			className="u-mt-16 u-mb-16"
+			start={Math.max(0, filters.page - 1) * ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE}
+			total={materialRequests?.total || 0}
+			count={ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE}
+			onPageChange={(pageZeroBased: number) => onPageChange(pageZeroBased, gotoPage)}
+		/>
+	);
+
+	const renderEmptyMessage = (): ReactNode =>
+		tHtml('pages/admin/materiaalaanvragen/index___geen-materiaalaanvragen');
+
+	const renderContent = (): ReactNode => {
+		return (
+			<Table<MaterialRequest>
+				className="u-mt-24 p-material-requests__table"
+				options={{
+					columns: getAdminMaterialRequestTableColumns(),
+					data: materialRequests?.items || [],
+					initialState: {
+						pageSize: ADMIN_MATERIAL_REQUESTS_TABLE_PAGE_SIZE,
+						sortBy: sortFilters,
+					} as TableState<MaterialRequest>,
+				}}
+				sortingIcons={sortingIcons}
+				pagination={renderPagination}
+				onSortChange={onSortChange}
+			/>
+		);
+	};
 
 	const renderPageContent = () => {
 		return (
 			<AdminLayout
 				pageTitle={tText('pages/admin/materiaalaanvragen/index___materiaalaanvragen')}
 			>
-				<AdminLayout.Content />
+				<AdminLayout.Content>
+					<div className={clsx('l-container l-container--edgeless-to-lg')}>
+						<SearchBar
+							id={globalLabelKeys.adminLayout.title}
+							default={filters.search}
+							placeholder={tText('pages/admin/materiaalaanvragen/index___zoek')}
+							onSearch={(value) => setFilters({ search: value, page: 1 })}
+						/>
+					</div>
+					<div
+						className={clsx('l-container l-container--edgeless-to-lg', {
+							'u-text-center u-color-neutral u-py-48': isFetching || noData,
+						})}
+					>
+						{isFetching && <Loading owner="Material requests overview" />}
+						{noData && renderEmptyMessage()}
+						{!noData && !isFetching && renderContent()}
+					</div>
+				</AdminLayout.Content>
 			</AdminLayout>
 		);
 	};
