da61db7741817b3fa6db157e7c4f7bc00be0c16d
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-03-02 21:30:41 +0100

    feat(RefinableRadioButton): move mock data

--- src/modules/shared/components/RefinableRadioButton/RefinableRadioButton.stories.tsx
+++ src/modules/shared/components/RefinableRadioButton/RefinableRadioButton.stories.tsx
@@ -3,40 +3,12 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
 import { RefinableRadioButton } from './RefinableRadioButton';
+import { MOCK_OPTIONS } from './RefinableRadioButton.mock';
 
 export default {
 	title: 'Components/RefinableRadioButton',
 	component: RefinableRadioButton,
 } as ComponentMeta<typeof RefinableRadioButton>;
-
-const MOCK_OPTIONS = [
-	{
-		id: 'type-1',
-		label: 'type 1',
-	},
-	{
-		id: 'type-2',
-		label: 'type 2',
-		refine: {
-			info: 'Info label',
-			options: [
-				{
-					id: 'refine-option-1',
-					label: 'refine-option-1',
-				},
-				{
-					id: 'refine-option-2',
-					label: 'refine-option-2',
-				},
-				{
-					id: 'refine-option-3',
-					label: 'refine-option-3',
-				},
-			],
-			label: 'dynamic label',
-		},
-	},
-];
 
 const Template: ComponentStory<typeof RefinableRadioButton> = (args) => (
 	<RefinableRadioButton {...args} />
