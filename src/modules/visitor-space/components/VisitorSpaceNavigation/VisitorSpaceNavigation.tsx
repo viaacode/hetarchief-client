f03ce52ecf618d3192d1c61016ca8b56848f654e
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-03-29 11:20:41 +0200

    feat(visitorSpaceNavigation): clean up render fn

--- src/modules/visitor-space/components/VisitorSpaceNavigation/VisitorSpaceNavigation.tsx
+++ src/modules/visitor-space/components/VisitorSpaceNavigation/VisitorSpaceNavigation.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@meemoo/react-components';
 import clsx from 'clsx';
-import { FC } from 'react';
+import { isNil } from 'lodash-es';
+import { FC, ReactNode } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Permission } from '@account/const';
@@ -19,7 +20,7 @@ const VisitorSpaceNavigation: FC<VisitorSpaceNavigationProps> = ({
 	className,
 	email,
 	phone,
-	showAccessEndDate,
+	accessEndDate,
 	title,
 }) => {
 	const { tHtml, tText } = useTranslation();
@@ -27,6 +28,72 @@ const VisitorSpaceNavigation: FC<VisitorSpaceNavigationProps> = ({
 	const showLinkedSpaceAsHomepage = useHasAllPermission(Permission.SHOW_LINKED_SPACE_AS_HOMEPAGE);
 	// Check if the url is of the format: /vrt and not of the format: /vrt/some-id
 	const isSearchPage = isVisitorSpaceSearchPage(window.location.pathname);
+
+	const renderContact = (): ReactNode => {
+		if (!phone && !email) {
+			return (
+				<span className="u-py-8">
+					{tHtml(
+						'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___geen-contactinformatie-beschikbaar'
+					)}
+				</span>
+			);
+		}
+
+		return (
+			<DropdownMenu
+				placement="bottom-end"
+				triggerButtonProps={{
+					className: clsx(
+						'u-color-white',
+						'u-px-12',
+						styles['c-visitor-space-navigation__contact-button']
+					),
+					icon: undefined,
+					iconStart: (
+						<Icon
+							className="u-font-size-24"
+							name={IconNamesLight.Contact}
+							aria-hidden
+						/>
+					),
+					label: tHtml(
+						'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___contacteer'
+					),
+					'aria-label': tText(
+						'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___contacteer'
+					),
+					variants: 'text',
+				}}
+			>
+				<ul className={styles['c-visitor-space-navigation__contact-list']}>
+					{email && (
+						<li className={styles['c-visitor-space-navigation__contact-item']}>
+							<Button
+								className="u-text-left"
+								variants={['text', 'block', 'sm']}
+								label={email}
+							/>
+
+							<CopyButton text={email} variants={['sm', 'text']} />
+						</li>
+					)}
+
+					{phone && (
+						<li className={styles['c-visitor-space-navigation__contact-item']}>
+							<Button
+								className="u-text-left"
+								variants={['text', 'block', 'sm']}
+								label={phone}
+							/>
+
+							<CopyButton text={phone} variants={['sm', 'text']} />
+						</li>
+					)}
+				</ul>
+			</DropdownMenu>
+		);
+	};
 
 	return (
 		<Navigation contextual className={className} showBorder={showBorder}>
@@ -44,66 +111,10 @@ const VisitorSpaceNavigation: FC<VisitorSpaceNavigationProps> = ({
 			<Navigation.Center title={title} />
 
 			<Navigation.Right placement="right">
-				{showAccessEndDate !== undefined ? (
-					<span className="u-py-8 u-text-right">{showAccessEndDate}</span>
-				) : !!phone || !!email ? (
-					<DropdownMenu
-						placement="bottom-end"
-						triggerButtonProps={{
-							className: clsx(
-								'u-color-white',
-								'u-px-12',
-								styles['c-visitor-space-navigation__contact-button']
-							),
-							icon: undefined,
-							iconStart: (
-								<Icon
-									className="u-font-size-24"
-									name={IconNamesLight.Contact}
-									aria-hidden
-								/>
-							),
-							label: tHtml(
-								'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___contacteer'
-							),
-							'aria-label': tText(
-								'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___contacteer'
-							),
-							variants: 'text',
-						}}
-					>
-						<ul className={styles['c-visitor-space-navigation__contact-list']}>
-							{email && (
-								<li className={styles['c-visitor-space-navigation__contact-item']}>
-									<Button
-										className="u-text-left"
-										variants={['text', 'block', 'sm']}
-										label={email}
-									/>
-
-									<CopyButton text={email} variants={['sm', 'text']} />
-								</li>
-							)}
-
-							{phone && (
-								<li className={styles['c-visitor-space-navigation__contact-item']}>
-									<Button
-										className="u-text-left"
-										variants={['text', 'block', 'sm']}
-										label={phone}
-									/>
-
-									<CopyButton text={phone} variants={['sm', 'text']} />
-								</li>
-							)}
-						</ul>
-					</DropdownMenu>
+				{!isNil(accessEndDate) ? (
+					<span className="u-py-8 u-text-right">{accessEndDate}</span>
 				) : (
-					<span className="u-py-8">
-						{tHtml(
-							'modules/visitor-space/components/visitor-space-navigation/visitor-space-navigation___geen-contactinformatie-beschikbaar'
-						)}
-					</span>
+					renderContact()
 				)}
 			</Navigation.Right>
 		</Navigation>
