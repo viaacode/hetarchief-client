ed02402366a373396977e1009865583cd3a70072
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-04-07 15:10:56 +0200

    feat(campaign-monitor-service): get and set newsletter preferences

---
+++ src/modules/account/types/account.ts
@@ -0,0 +1,11 @@
+
+export interface GetNewsletterPreferencesResponse {
+	newsletter: boolean;
+}
+
+export interface SetNewsletterPreferencesBody {
+	firstName: string;
+	lastName: string;
+	mail: string;
+	preferences: GetNewsletterPreferencesResponse;
+}
