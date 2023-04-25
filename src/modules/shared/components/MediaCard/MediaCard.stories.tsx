cfa7c94e233d552bb6399bb355a33e0a56270ddc
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-04-25 11:37:41 +0200

    fix(arc-1574): show creation date if published date is undefined

--- src/modules/shared/components/MediaCard/MediaCard.stories.tsx
+++ src/modules/shared/components/MediaCard/MediaCard.stories.tsx
@@ -56,7 +56,7 @@ const Template: ComponentStory<typeof MediaCard> = (args) => {
 export const Grid: ComponentStory<typeof MediaCard> = Template.bind({});
 Grid.args = {
 	view: 'grid',
-	publishedAt: new Date(),
+	publishedOrCreatedDate: new Date(),
 	publishedBy: 'Studio Hyperdrive',
 	description:
 		'Donec nunc odio, fermentum vel laoreet at, luctus lobortis mauris. Nullam metus lectus, semper vitae nisi in, mollis porttitor nisi. Vivamus pharetra accumsan congue. Aenean commodo leo felis, a placerat velit facilisis non. Proin sollicitudin ultrices mi in viverra. Nam gravida, neque vel porttitor malesuada, velit mi placerat sapien, in fermentum ante neque vitae odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec imperdiet nibh in nisi sagittis, et fermentum turpis dictum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla sollicitudin ipsum eget fermentum iaculis. Nunc facilisis nibh orci, eget euismod lacus suscipit et. Maecenas et tellus consectetur, ornare elit vitae, ullamcorper dolor. Proin vel semper quam. Quisque consequat orci nec semper facilisis. ',
