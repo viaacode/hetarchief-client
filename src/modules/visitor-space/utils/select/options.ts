6889ac19628b21f49be68f945f96cf6d86391948
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-05-04 16:11:04 +0200

    fix(arc-1213): apply pr remark

--- src/modules/visitor-space/utils/select/options.ts
+++ src/modules/visitor-space/utils/select/options.ts
@@ -1,12 +1,14 @@
 import { SelectOption } from '@meemoo/react-components';
 
+import { tText } from '@shared/helpers/translate';
+
 export const getSelectValue = (
 	options: SelectOption[],
 	optionValue: string | undefined | null
 ): SelectOption | undefined => {
 	if (optionValue === undefined) {
 		return {
-			label: 'Select...',
+			label: tText('modules/visitor-space/utils/select/options___select'),
 			value: '',
 		};
 	}
