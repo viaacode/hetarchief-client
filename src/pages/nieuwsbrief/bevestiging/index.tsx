1a5185d40962012bfc3edf64bee58caa1995b085
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-05-08 13:31:24 +0200

    feat(nieuwsbrief): add confirmation and failed flow

--- src/pages/nieuwsbrief/bevestiging/index.tsx
+++ src/pages/nieuwsbrief/bevestiging/index.tsx
@@ -1,24 +1,46 @@
 import { GetServerSidePropsResult, NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { GetServerSidePropsContext } from 'next/types';
+import { useEffect, useState } from 'react';
 
+import { Loading } from '@shared/components';
+import { ROUTES } from '@shared/const';
 import { getDefaultServerSideProps } from '@shared/helpers/get-default-server-side-props';
 import { renderOgTags } from '@shared/helpers/render-og-tags';
 import { useHideFooter } from '@shared/hooks/use-hide-footer';
 import useTranslation from '@shared/hooks/use-translation/use-translation';
+import { toastService } from '@shared/services/toast-service';
 import { DefaultSeoInfo } from '@shared/types/seo';
 
 const NewsletterConfirmation: NextPage<DefaultSeoInfo> = ({ url }) => {
 	useHideFooter();
 
-	const { tText } = useTranslation();
+	const { tText, tHtml } = useTranslation();
+	const router = useRouter();
 
-	const renderPageContent = () => {
-		return (
-			<>
-				<h1>NIEUWSBRIEF PAGINA - BEVESTIGING</h1>
-			</>
-		);
-	};
+	const [triggerRedirect, setTriggerRedirect] = useState(false);
+
+	useEffect(() => {
+		toastService.notify({
+			maxLines: 3,
+			title: tHtml(
+				'pages/nieuwsbrief-bevestiging/index___nieuwsbrief-bevestiging___bevestiging'
+			),
+			description: tHtml(
+				'pages/nieuwsbrief-bevestiging/index___nieuwsbrief-bevestiging___inschrijving-op-de-nieuwsbrief-is-gelukt'
+			),
+		});
+
+		setTriggerRedirect(true);
+	}, []);
+
+	useEffect(() => {
+		if (!triggerRedirect) {
+			return;
+		}
+
+		router.replace(`/${ROUTES.home}`);
+	}, [router, triggerRedirect]);
 
 	return (
 		<div className="p-newsletter-confirmation">
@@ -28,7 +50,7 @@ const NewsletterConfirmation: NextPage<DefaultSeoInfo> = ({ url }) => {
 				url
 			)}
 
-			{renderPageContent()}
+			<Loading owner="nieuwsbrief bevestiging" fullscreen />
 		</div>
 	);
 };
--- src/pages/nieuwsbrief/mislukt/index.tsx
+++ src/pages/nieuwsbrief/mislukt/index.tsx
@@ -1,24 +1,44 @@
 import { GetServerSidePropsResult, NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { GetServerSidePropsContext } from 'next/types';
+import { useEffect, useState } from 'react';
 
+import { Loading } from '@shared/components';
+import { ROUTES } from '@shared/const';
 import { getDefaultServerSideProps } from '@shared/helpers/get-default-server-side-props';
 import { renderOgTags } from '@shared/helpers/render-og-tags';
 import { useHideFooter } from '@shared/hooks/use-hide-footer';
 import useTranslation from '@shared/hooks/use-translation/use-translation';
+import { toastService } from '@shared/services/toast-service';
 import { DefaultSeoInfo } from '@shared/types/seo';
 
 const NewsletterFailed: NextPage<DefaultSeoInfo> = ({ url }) => {
 	useHideFooter();
 
-	const { tText } = useTranslation();
+	const { tText, tHtml } = useTranslation();
+	const router = useRouter();
 
-	const renderPageContent = () => {
-		return (
-			<>
-				<h1>NIEUWSBRIEF PAGINA - MISLUKT</h1>
-			</>
-		);
-	};
+	const [triggerRedirect, setTriggerRedirect] = useState(false);
+
+	useEffect(() => {
+		toastService.notify({
+			maxLines: 3,
+			title: tHtml('pages/nieuwsbrief-mislukt/index___nieuwsbrief-mislukt___mislukt'),
+			description: tHtml(
+				'pages/nieuwsbrief-mislukt/index___nieuwsbrief-mislukt___inschrijving-op-de-nieuwsbrief-is-mislukt'
+			),
+		});
+
+		setTriggerRedirect(true);
+	}, []);
+
+	useEffect(() => {
+		if (!triggerRedirect) {
+			return;
+		}
+
+		router.replace(`/${ROUTES.home}`);
+	}, [router, triggerRedirect]);
 
 	return (
 		<div className="p-newsletter-failed">
@@ -28,7 +48,7 @@ const NewsletterFailed: NextPage<DefaultSeoInfo> = ({ url }) => {
 				url
 			)}
 
-			{renderPageContent()}
+			<Loading fullscreen owner="nieuwsbrief mislukt" />
 		</div>
 	);
 };
