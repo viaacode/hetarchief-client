5a6e822d5c1304d0cbab9e424fd46df1d3f1e9fb
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-05-12 10:06:32 +0200

    fix(arc-1687): redirect to searchpage is kioskuser

--- src/modules/auth/wrappers/with-auth/with-auth.tsx
+++ src/modules/auth/wrappers/with-auth/with-auth.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { stringify } from 'query-string';
 import { ComponentType, useCallback, useEffect, useState } from 'react';
 
+import { GroupName } from '@account/const';
 import { AuthMessage, AuthService } from '@auth/services/auth-service';
 import Loading from '@shared/components/Loading/Loading';
 import { ROUTES } from '@shared/const';
@@ -50,6 +51,10 @@ export const withAuth = (
 				);
 			};
 
+			const toSearchPage = async () => {
+				return router.replace(ROUTES.search);
+			};
+
 			if (!login?.userInfo || login.message === AuthMessage.LoggedOut) {
 				// User is logged out
 				if (isLoginRequired) {
@@ -61,6 +66,10 @@ export const withAuth = (
 				}
 			} else {
 				// User is logged in
+				if (login.userInfo.groupName === GroupName.KIOSK_VISITOR) {
+					// User is kioskuser
+					await toSearchPage();
+				}
 				if (isCurrentTosAccepted(login.userInfo.acceptedTosAt, tos?.updatedAt)) {
 					// User has accepted the latest version of the terms of service
 					setShowPage(true);
