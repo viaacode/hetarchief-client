bfec0734f6018a5a65e6cb4bfa67433be13b4b79
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-05-25 08:56:10 +0200

    feat(events-service): remove todo

--- src/modules/shared/services/events-service/events.service.types.ts
+++ src/modules/shared/services/events-service/events.service.types.ts
@@ -2,7 +2,7 @@ export enum LogEventType {
 	// Triggered in FE
 	BEZOEK_ITEM_VIEW = 'be.hetarchief.bezoek.item.view',
 	BEZOEK_ITEM_PLAY = 'be.hetarchief.bezoek.item.play',
-	ITEM_REQUEST = 'be.hetarchief.item.request', // ToDo(Silke): ook versturen voor VRT en UGent, met de info die op dat moment beschikbaar is en de rest leeg laten (dus wel info over het item en de gebruiker, maar niet de info die in het formulier wordt ingevoerd
+	ITEM_REQUEST = 'be.hetarchief.item.request',
 	ITEM_VIEW = 'be.hetarchief.item.view',
 	ITEM_PLAY = 'be.hetarchief.item.play',
 	SEARCH = 'be.hetarchief.item.search',
