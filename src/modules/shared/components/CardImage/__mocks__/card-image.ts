51392bed54fbd49e0659d9decdf717b47d6f7983
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-06-09 10:02:01 +0200

    fix(arc-1792): add final newsletter background
    https://meemoo.atlassian.net/browse/ARC-1792

--- src/modules/shared/components/CardImage/__mocks__/card-image.ts
+++ src/modules/shared/components/CardImage/__mocks__/card-image.ts
@@ -2,8 +2,8 @@ import { CardImageProps } from '../CardImage.types';
 
 export const cardImageMock: CardImageProps = {
 	color: '#220538',
-	image: '/images/bg-shd.png',
-	logo: '/images/logo-shd--small.svg',
+	image: '/images/bg-newsletter.png',
+	logo: '/images/logo_meemoo.svg',
 	name: 'Studio Hyperdrive',
 	id: 'SHD',
 	size: 'short',
