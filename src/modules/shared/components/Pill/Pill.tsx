0695bba1b8367c8a09deb2f8c5a6d22bb101f169
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-06-29 10:04:33 +0200

    fix(arc-1711): if mobile show tooltip on rightside

--- src/modules/shared/components/Pill/Pill.tsx
+++ src/modules/shared/components/Pill/Pill.tsx
@@ -2,19 +2,25 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@meemoo/react-component
 import clsx from 'clsx';
 import { FC, ReactElement } from 'react';
 
+import { useWindowSizeContext } from '@shared/hooks/use-window-size-context';
+import { Breakpoints } from '@shared/types';
+
 import { Icon } from '..';
 
 import styles from './Pill.module.scss';
 import { PillProps } from './Pill.types';
 
 const Pill: FC<PillProps> = ({ className, icon, label, isExpanded }: PillProps): ReactElement => {
+	const windowSize = useWindowSizeContext();
+	const isMobile = !!(windowSize.width && windowSize.width < Breakpoints.md);
+
 	const rootCls = clsx(className, styles['c-pill'], {
 		[styles['c-pill--expanded']]: isExpanded,
 	});
 
 	const renderTooltipPill = (): ReactElement => (
 		<span className={rootCls}>
-			<Tooltip position="top">
+			<Tooltip position={isMobile ? 'right' : 'top'}>
 				<TooltipTrigger>
 					<Icon name={icon} />
 				</TooltipTrigger>
