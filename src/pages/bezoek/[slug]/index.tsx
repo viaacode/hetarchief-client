99f740bb0a2711a6772084cbc7c07fc8d668c029
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-07-19 09:52:07 +0200

    fix(arc-1522): fix redirects /bezoek/[slug]

--- src/pages/bezoek/[slug]/index.tsx
+++ src/pages/bezoek/[slug]/index.tsx
@@ -3,8 +3,10 @@ import { GetServerSidePropsResult, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { GetServerSidePropsContext } from 'next/types';
 import { stringifyUrl } from 'query-string';
-import { ComponentType, useEffect } from 'react';
+import { ComponentType, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import { selectIsLoggedIn } from '@auth/store/user';
 import { ErrorNoAccessToObject, ErrorNotFound, Loading } from '@shared/components';
 import { ErrorSpaceNoLongerActive } from '@shared/components/ErrorSpaceNoLongerActive';
 import { ROUTES } from '@shared/const';
@@ -29,6 +31,8 @@ const VisitPage: NextPage<VisitPageProps> = () => {
 	const { tText } = useTranslation();
 	const router = useRouter();
 	const { slug } = router.query;
+	const isLoggedIn = useSelector(selectIsLoggedIn);
+	const [showNoAccess, setShowNoAccess] = useState(false);
 
 	/**
 	 * Data
@@ -72,6 +76,12 @@ const VisitPage: NextPage<VisitPageProps> = () => {
 		}
 
 		if (!isLoadingAccessStatus && !isLoadingSpaceInfo && !hasAccess && !isErrorSpaceNotFound) {
+			// If not logged in show no access page
+			if (!isLoggedIn || !hasAccess) {
+				setShowNoAccess(true);
+				return;
+			}
+
 			// No access to the visitor space, but the maintainer exists => so we can redirect to the search page
 			router.push(
 				stringifyUrl({
@@ -101,6 +111,18 @@ const VisitPage: NextPage<VisitPageProps> = () => {
 	 */
 
 	const renderPageContent = () => {
+		if (showNoAccess) {
+			return (
+				<ErrorNoAccessToObject
+					description={tText(
+						'pages/bezoek/slug/index___je-hebt-geen-toegang-tot-deze-bezoekersruimte-vraag-toegang-aan-of-doorzoek-de-publieke-catalogus'
+					)}
+					visitorSpaceName={(slug || null) as string | null}
+					visitorSpaceSlug={(slug || null) as string | null}
+				/>
+			);
+		}
+
 		if (
 			isLoadingAccessStatus ||
 			isLoadingSpaceInfo ||
