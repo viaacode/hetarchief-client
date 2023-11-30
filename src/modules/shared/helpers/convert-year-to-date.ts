6d685364f2c13ce755232926d6a22b13f2dbd26f
Bert Verhelst <verhelstbert@gmail.com>
2023-11-30 14:20:03 +0100

    fix(ARC-2026): fix year input filters
    https://meemoo.atlassian.net/browse/ARC-2026

--- src/modules/shared/helpers/convert-year-to-date.ts
+++ src/modules/shared/helpers/convert-year-to-date.ts
@@ -2,8 +2,11 @@ import { SEPARATOR } from '@shared/const';
 import { Operator } from '@shared/types';
 
 export function convertYearToDate(yearString: string, operator: Operator): string {
-	const startOfYear = `${yearString}-01-01T00:00:00Z`;
-	const endOfYear = `${yearString}-12-31T23:59:59Z`;
+	// No trailing Z since we can't these dates to be interpreted as belgian time, not utc time
+	// Since we only have the year and the year is entered as belgian time
+	// otherwise end of year jumps from 31/12/2009 => 01/01/2010
+	const startOfYear = `${yearString}-01-01T00:00:00`;
+	const endOfYear = `${yearString}-12-31T23:59:59`;
 
 	if (operator === Operator.Equals) {
 		return `${startOfYear}${SEPARATOR}${endOfYear}`;
