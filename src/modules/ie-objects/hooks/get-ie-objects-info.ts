10d26354e451d5a7081d61d14b02108094d0da3d
Robbe Bierebeeck <robbe.bierebeeck@studiohyperdrive.be>
2024-08-23 08:58:17 +0200

    feat: redirect old pid to new pid

--- src/modules/ie-objects/hooks/get-ie-objects-info.ts
+++ src/modules/ie-objects/hooks/get-ie-objects-info.ts
@@ -1,4 +1,5 @@
 import { type QueryClient, useQuery, type UseQueryResult } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 
 import { QUERY_KEYS } from '@shared/const/query-keys';
 
@@ -16,11 +17,28 @@ export const useGetIeObjectInfo = (
 		enabled?: boolean;
 	} = {}
 ): UseQueryResult<IeObject | null> => {
-	return useQuery([QUERY_KEYS.getIeObjectsInfo, id], () => getIeObjectInfo(id), {
-		keepPreviousData: true,
-		enabled: true,
-		...options,
-	});
+	const router = useRouter();
+	return useQuery(
+		[QUERY_KEYS.getIeObjectsInfo, id],
+		async () => {
+			let newId;
+			if (id.length > 36) {
+				const v3Identifier = await IeObjectsService.schemaIdentifierLookup(id);
+				newId = v3Identifier.schemaIdentifierV3;
+				const url = new URL(window.location.href);
+				url.pathname = url.pathname.replace(id, newId);
+				await router.replace(url.toString());
+			} else {
+				newId = id;
+			}
+			return getIeObjectInfo(newId);
+		},
+		{
+			keepPreviousData: true,
+			enabled: true,
+			...options,
+		}
+	);
 };
 
 export function makeServerSideRequestGetIeObjectInfo(
