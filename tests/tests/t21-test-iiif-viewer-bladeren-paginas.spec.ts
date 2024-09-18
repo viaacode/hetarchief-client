bbe570aaf9a9f246ed0d90c216bb1171375520eb
Bert Verhelst <verhelstbert@gmail.com>
2024-09-18 15:54:39 +0200

    feat(ARC-2279): add end-2-end test to test browsing newspaper pages
    https://meemoo.atlassian.net/issues/ARC-2279

---
+++ tests/tests/t21-test-iiif-viewer-bladeren-paginas.spec.ts
@@ -0,0 +1,70 @@
+import { expect, test } from '@playwright/test';
+
+import { goToPageAndAcceptCookies } from '../helpers/go-to-page-and-accept-cookies';
+
+test('T21: iiif viewer bladeren paginas', async ({ page, context }) => {
+	/**
+	 * Go to a newspaper detail page ---------------------------------------------------------------
+	 */
+	await goToPageAndAcceptCookies(
+		page,
+		(process.env.TEST_CLIENT_ENDPOINT as string) + '/pid/h98z893q54',
+		'Wet- en verordeningsblad voor de bezette streke... | hetarchief.be'
+	);
+
+	// Go to page again to fix non-loading newspaper in incognito browser
+	await page.goto((process.env.TEST_CLIENT_ENDPOINT as string) + '/pid/h98z893q54');
+
+	// Navigation with thumbnail buttons ---------------------------------------------------------------
+
+	// Check second page thumbnail button is visible
+	const secondPageThumbnail = page.locator(
+		'[class*="IiifViewer_c-iiif-viewer__iiif__reference-strip__"] [alt="page 2"]'
+	);
+	await expect(secondPageThumbnail).toBeVisible();
+
+	// Click second page thumbnail button
+	await secondPageThumbnail.click();
+
+	// Check page url changed
+	expect(page.url()).toContain('activePage=1');
+
+	// Check first page thumbnail button is visible
+	const firstPageThumbnail = page.locator(
+		'[class*="IiifViewer_c-iiif-viewer__iiif__reference-strip__"] [alt="page 1"]'
+	);
+	await expect(firstPageThumbnail).toBeVisible();
+
+	// Click first page thumbnail button
+	await firstPageThumbnail.click();
+
+	// Check page url changed
+	expect(page.url()).toContain('activePage=0');
+
+	// Navigation with previous/next page buttons ---------------------------------------------------------------
+
+	// Check next page button is visible
+	const nextPageButton = page.locator('.c-iiif-viewer__iiif__controls__grid-view__next-image');
+	await expect(nextPageButton).toBeVisible();
+
+	// Click second page thumbnail button
+	await nextPageButton.click();
+
+	// Check page url changed
+	expect(page.url()).toContain('activePage=1');
+
+	// Check previous page button is visible
+	const previousPageButton = page.locator(
+		'.c-iiif-viewer__iiif__controls__grid-view__previous-image'
+	);
+	await expect(previousPageButton).toBeVisible();
+
+	// Click first page thumbnail button
+	await previousPageButton.click();
+
+	// Check page url changed
+	expect(page.url()).toContain('activePage=0');
+
+	// Wait for close to save the videos
+	await context.close();
+});
