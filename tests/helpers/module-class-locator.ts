7aa2c4175eee789b4f9a0738e8d2a787e30741ef
Bert Verhelst <verhelstbert@gmail.com>
2024-10-04 10:31:52 +0200

    feat(ARC-2282): add test for downloading newspaper in multiple formats
    https://meemoo.atlassian.net/browse/ARC-2282

--- tests/helpers/module-class-locator.ts
+++ tests/helpers/module-class-locator.ts
@@ -1,9 +1,10 @@
 /**
  * Convert a simple class name to a module class selector
  * eg:
- * moduleClassSelector('c-button') => '[class*="_c-button__"],[class*="_c-button--"]'
+ * moduleClassSelector('c-button') => '[class*="_c-button__"]'
  * @param className
+ * @param suffix
  */
-export function moduleClassSelector(className: string): string {
-	return `[class*="_${className}__"],[class*="_${className}--"]`;
+export function moduleClassSelector(className: string, suffix: string = '__'): string {
+	return `[class*="_${className}${suffix}"]`;
 }
