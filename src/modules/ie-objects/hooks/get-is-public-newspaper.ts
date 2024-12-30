e0a1e9b3ae1d55d497e4204fb84c3e8519a876aa
Bert Verhelst <verhelstbert@gmail.com>
2024-12-30 16:12:43 +0100

    fix(ARC-2617): show download for public objects to logged out users
    https://meemoo.atlassian.net/browse/ARC-2617

---
+++ src/modules/ie-objects/hooks/get-is-public-newspaper.ts
@@ -0,0 +1,16 @@
+import { IeObjectType } from '@shared/types/ie-objects';
+
+import { type IeObject, IeObjectLicense } from './../ie-objects.types';
+
+export const useIsPublicNewspaper = (mediaInfo: IeObject | null | undefined): boolean => {
+	if (!mediaInfo) {
+		return false;
+	}
+	const hasPublicLicense = !!mediaInfo?.licenses?.includes(IeObjectLicense.PUBLIEK_CONTENT);
+	const hasPublicCopyright =
+		!!mediaInfo?.licenses?.includes(IeObjectLicense.PUBLIC_DOMAIN) ||
+		!!mediaInfo?.licenses?.includes(IeObjectLicense.COPYRIGHT_UNDETERMINED);
+	return (
+		hasPublicLicense && hasPublicCopyright && mediaInfo.dctermsFormat === IeObjectType.Newspaper
+	);
+};
