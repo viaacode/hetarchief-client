73c6febfa08ddee8660916356dd5721f1762094a
Bert Verhelst <verhelstbert@gmail.com>
2025-04-11 15:34:28 +0200

    fix(ARC-2839): fix percentage in confidence indicator for names list
    https://meemoo.atlassian.net/browse/ARC-2839

--- src/modules/ie-objects/components/ConfidenceIndicator/ConfidenceIndicator.tsx
+++ src/modules/ie-objects/components/ConfidenceIndicator/ConfidenceIndicator.tsx
@@ -8,14 +8,15 @@ import { tText } from '@shared/helpers/translate';
 import styles from './ConfidenceIndicator.module.scss';
 
 export const ConfidenceIndicator: FC<ConfidenceIndicatorProps> = ({ className, confidence }) => {
+	confidence = 0.8;
 	return (
 		<Tooltip position="left">
 			<TooltipTrigger>
 				<div className={clsx(styles['c-confidence-indicator'], className)}>
 					<div className={styles['c-confidence-indicator__bars-container']}>
-						<span className={confidence > 75 ? 'active' : undefined} />
-						<span className={confidence > 50 ? 'active' : undefined} />
-						<span className={confidence > 25 ? 'active' : undefined} />
+						<span className={confidence > 0.75 ? 'active' : undefined} />
+						<span className={confidence > 0.5 ? 'active' : undefined} />
+						<span className={confidence > 0.25 ? 'active' : undefined} />
 					</div>
 
 					<span>{Math.ceil(confidence * 100)}%</span>
@@ -25,7 +26,7 @@ export const ConfidenceIndicator: FC<ConfidenceIndicatorProps> = ({ className, c
 				{tText(
 					'modules/ie-objects/components/confidence-indicator/confidence-indicator___we-zijn-confidence-zeker-dat-deze-naam-voorkomt-in-deze-krant',
 					{
-						confidence,
+						confidence: Math.ceil(confidence * 100),
 					}
 				)}
 			</TooltipContent>
