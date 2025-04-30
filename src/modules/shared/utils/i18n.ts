36214e2f71fc3e68cf14eaf8a203af0ba2962070
Bert Verhelst <verhelstbert@gmail.com>
2025-04-30 17:31:56 +0200

    fix(ARC-2943): translate date on my material requests x time ago
    https://meemoo.atlassian.net/browse/ARC-2943

--- src/modules/shared/utils/i18n.ts
+++ src/modules/shared/utils/i18n.ts
@@ -1,8 +1,15 @@
 import type { Locale as DateFnsLocale } from 'date-fns';
+import enGB from 'date-fns/locale/en-GB/index.js';
 import nlBE from 'date-fns/locale/nl-BE/index.js';
 
-export function getLocaleFromI18nLanguage(language: string): DateFnsLocale {
+export function getLocaleFromI18nLanguage(language: Locale): DateFnsLocale {
 	switch (language) {
+		case 'en':
+			return enGB;
+
+		case 'nl':
+			return nlBE;
+
 		default:
 			return nlBE;
 	}
