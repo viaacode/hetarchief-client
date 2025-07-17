77f89b715c200579eeaf0dd03eab0a0a559aec72
Bert Verhelst <verhelstbert@gmail.com>
2025-07-17 09:25:36 +0200

    fix(ARC-3112): switch the advanced filters tooltip popup behavior
    https://meemoo.atlassian.net/browse/ARC-3112

--- src/modules/shared/components/TagSearchBar/TagSearchBarInfo/TagSearchBarInfo.tsx
+++ src/modules/shared/components/TagSearchBar/TagSearchBarInfo/TagSearchBarInfo.tsx
@@ -1,15 +1,48 @@
-import type { FC } from 'react';
+import { type FC, useRef, useState } from 'react';
 
 import { Icon } from '../../Icon';
 
+import { useClickOutside } from '@meemoo/react-components';
+import clsx from 'clsx';
 import styles from './TagSearchBarInfo.module.scss';
 import type { TagSearchBarInfoProps } from './TagSearchBarInfo.types';
 
-const TagSearchBarInfo: FC<TagSearchBarInfoProps> = ({ icon, content }) => (
-	<aside className={styles['c-tag-search-bar-info']}>
-		<Icon name={icon} className={styles['c-tag-search-bar-info__icon']} />
-		<div className={styles['c-tag-search-bar-info__content']}>{content}</div>
-	</aside>
-);
+const TagSearchBarInfo: FC<TagSearchBarInfoProps> = ({ icon, content }) => {
+	const [showContent, setShowContent] = useState(false);
+	const tagSearchBarInfoRef = useRef<HTMLDivElement>(null);
+	const [isTimeoutClickAfterHover, setIsTimeoutClickAfterHover] = useState(false);
+
+	useClickOutside(tagSearchBarInfoRef.current as Element, () => setShowContent(false));
+
+	const toggleContent = () => {
+		if (isTimeoutClickAfterHover) {
+			// Prevent toggling content if the user clicked right after hovering the button
+			return;
+		}
+		setShowContent(!showContent);
+	};
+
+	return (
+		<aside
+			className={clsx(
+				styles['c-tag-search-bar-info'],
+				showContent && styles['c-tag-search-bar-info--open']
+			)}
+			ref={tagSearchBarInfoRef}
+		>
+			<Icon
+				name={icon}
+				className={styles['c-tag-search-bar-info__icon']}
+				onClick={toggleContent}
+				onMouseEnter={() => {
+					setIsTimeoutClickAfterHover(true);
+					setShowContent(true);
+					setTimeout(() => setIsTimeoutClickAfterHover(false), 1000);
+				}}
+			/>
+			{showContent && <div className={styles['c-tag-search-bar-info__content']}>{content}</div>}
+		</aside>
+	);
+};
 
 export default TagSearchBarInfo;
