114f09ece11e7370a1e7aed83bb2cba0f3b92187
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-01-29 14:31:44 +0100

    ARC-3318: Only hide the more info label but not the more info component on mobile

--- src/modules/navigation/components/MaterialRequestCenterBlade/MaterialRequestCenterBlade.tsx
+++ src/modules/navigation/components/MaterialRequestCenterBlade/MaterialRequestCenterBlade.tsx
@@ -173,7 +173,7 @@ const MaterialRequestCenterBlade: FC<MaterialRequestCenterBladeProps> = ({ isOpe
 					)}
 					{materialRequests?.length > 0 && ` (${materialRequests.length})`}
 				</h2>
-				<MaterialRequestInformation hideOnMobile={true} />
+				<MaterialRequestInformation />
 				<p className={styles['c-material-request-center-blade__more-info']}>
 					{tText(
 						'modules/navigation/components/material-request-center-blade/material-request-center-blade___vraag-dit-materiaal-rechtstreeks-aan-bij-de-aanbieder-s'
