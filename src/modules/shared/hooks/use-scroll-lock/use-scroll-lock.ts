0cd5a7bf44d6f1384ff64e9215ce097c62dd7a75
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-02-05 10:50:45 +0100

    bugfix: simplify scroll lock to prevent scrolling to top when

--- src/modules/shared/hooks/use-scroll-lock/use-scroll-lock.ts
+++ src/modules/shared/hooks/use-scroll-lock/use-scroll-lock.ts
@@ -1,14 +1,10 @@
+import { selectIsScrollLocked, setLockScroll } from '@shared/store/ui';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-import { selectIsScrollLocked, setLockScroll } from '@shared/store/ui';
-import { findParentByClass } from '@shared/utils/find-parent-by-class';
 
 import { useScrollbarWidth } from '../use-scrollbar-width';
 
 import type { UseScrollLock } from './use-scroll-lock.types';
-
-type scrollState = { __scrollDepth?: number };
 
 const useScrollLock: UseScrollLock = (lock, id) => {
 	// Ensure state is synced first, before any operations
@@ -26,84 +22,21 @@ const useScrollLock: UseScrollLock = (lock, id) => {
 	const isLocked = useSelector(selectIsScrollLocked);
 
 	/**
-	 * Methods
-	 */
-
-	// wrapper for window.scrollTo
-	const scroll = useCallback((behavior?: ScrollBehavior) => {
-		window.scrollTo({
-			top: (document as unknown as scrollState).__scrollDepth,
-			left: 0,
-			behavior,
-		});
-	}, []);
-
-	// Block wheel events outside of blade body (needed for Safari)
-	const preventWheel = useCallback(
-		(e?: WheelEvent) => {
-			const target = e?.target as HTMLElement | null;
-			const inBlade = findParentByClass('c-blade__body-wrapper', target);
-
-			if (inBlade) {
-				const { scrollHeight: total, clientHeight: size, scrollTop: depth } = inBlade;
-				const goingUp = (e?.deltaY || 0) < 0;
-				const goingDown = (e?.deltaY || 0) > 0;
-				const atBottom = total === size + depth;
-
-				if (goingUp || (goingDown && !atBottom)) {
-					return;
-				}
-			}
-
-			scroll('smooth');
-			e?.stopImmediatePropagation();
-		},
-		[scroll]
-	);
-
-	/**
 	 * Actions
 	 */
 
 	const disable = useCallback(
 		(el: HTMLElement) => {
-			// Use the document as a one-shot state machine
-			if (!(document as unknown as scrollState).__scrollDepth) {
-				(document as unknown as scrollState).__scrollDepth = window.scrollY;
-			}
-
-			el.style.overflowY = 'hidden';
+			el.style.overflow = 'hidden';
 			el.style.marginRight = `${scrollbarWidth}px`;
-			el.style.position = 'fixed';
-			el.style.width = '100vw';
-			el.style.overflowX = 'hidden';
-			el.style.top = `-${(document as unknown as scrollState).__scrollDepth}px`;
-			window.onwheel = preventWheel;
-
-			// Use that state to go to the right depth
-			scroll();
 		},
-		[scrollbarWidth, scroll, preventWheel]
+		[scrollbarWidth]
 	);
 
-	const enable = useCallback(
-		(el: HTMLElement) => {
-			el.style.overflowY = '';
-			el.style.marginRight = '';
-			el.style.position = 'relative';
-			el.style.top = '0';
-			window.onwheel = null;
-
-			scroll();
-
-			// Wipe our state asynchronously once every hook instance is done
-			// Known issue: when rapidly (<50ms) toggling blades, the scrollDepth is lost
-			setTimeout(() => {
-				(document as unknown as scrollState).__scrollDepth = undefined;
-			}, 50);
-		},
-		[scroll]
-	);
+	const enable = useCallback((el: HTMLElement) => {
+		el.style.overflow = '';
+		el.style.marginRight = '';
+	}, []);
 
 	/**
 	 * Switch
