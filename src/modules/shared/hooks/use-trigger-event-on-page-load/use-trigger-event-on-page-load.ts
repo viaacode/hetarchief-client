c220c3b768e62bd46e66c71aa16a847d9ba0a3f7
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-03-02 14:38:17 +0100

    ARC-3383: PR remarks

--- src/modules/shared/hooks/use-trigger-event-on-page-load/use-trigger-event-on-page-load.ts
+++ src/modules/shared/hooks/use-trigger-event-on-page-load/use-trigger-event-on-page-load.ts
@@ -16,14 +16,14 @@ export function useTriggerEventOnPageLoad({
 	eventData,
 	shouldTrigger = true,
 }: UseTriggerEventOnPageLoadParams) {
-	const [hasTriggered, setHasTriggered] = useState<Record<string, boolean>>({});
+	const [hasTriggeredForUrl, setHasTriggeredForUrl] = useState<Record<string, boolean>>({});
 	const [previewQueryParam] = useQueryParam(QUERY_PARAM_KEY.CONTENT_PAGE_PREVIEW, BooleanParam);
 
 	useEffect(() => {
-		if (!shouldTrigger || hasTriggered[window.location.href] || previewQueryParam) {
+		if (!shouldTrigger || hasTriggeredForUrl[window.location.href] || previewQueryParam) {
 			return;
 		}
 		EventsService.triggerEvent(eventType, window.location.href, eventData).then(noop);
-		setHasTriggered((prev) => ({ ...prev, [window.location.href]: true }));
-	}, [hasTriggered, shouldTrigger, previewQueryParam, eventType, eventData]);
+		setHasTriggeredForUrl((prev) => ({ ...prev, [window.location.href]: true }));
+	}, [hasTriggeredForUrl, shouldTrigger, previewQueryParam, eventType, eventData]);
 }
