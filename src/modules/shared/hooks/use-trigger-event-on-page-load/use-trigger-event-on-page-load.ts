f350a9592bce3ab313a920b4b3c29358aee2ddf5
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-03-02 09:20:57 +0100

    ARC-3383: hook should store trigger state based on href

--- src/modules/shared/hooks/use-trigger-event-on-page-load/use-trigger-event-on-page-load.ts
+++ src/modules/shared/hooks/use-trigger-event-on-page-load/use-trigger-event-on-page-load.ts
@@ -16,14 +16,14 @@ export function useTriggerEventOnPageLoad({
 	eventData,
 	shouldTrigger = true,
 }: UseTriggerEventOnPageLoadParams) {
-	const [hasTriggered, setHasTriggered] = useState<boolean>(false);
+	const [hasTriggered, setHasTriggered] = useState<Record<string, boolean>>({});
 	const [previewQueryParam] = useQueryParam(QUERY_PARAM_KEY.CONTENT_PAGE_PREVIEW, BooleanParam);
 
 	useEffect(() => {
-		if (!shouldTrigger || hasTriggered || previewQueryParam) {
+		if (!shouldTrigger || hasTriggered[window.location.href] || previewQueryParam) {
 			return;
 		}
 		EventsService.triggerEvent(eventType, window.location.href, eventData).then(noop);
-		setHasTriggered(true);
+		setHasTriggered((prev) => ({ ...prev, [window.location.href]: true }));
 	}, [hasTriggered, shouldTrigger, previewQueryParam, eventType, eventData]);
 }
