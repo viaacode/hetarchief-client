bcf3203ba80cd7abd266c6c3f8af34634d362c52
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-06-15 08:17:44 +0200

    ARC-3587: fix wrong logging

--- src/pages/bezoek/[slug]/index.tsx
+++ src/pages/bezoek/[slug]/index.tsx
@@ -1,12 +1,11 @@
-import type { GetServerSidePropsResult, NextPage } from 'next';
-import type { GetServerSidePropsContext } from 'next/types';
-import type { ComponentType } from 'react';
-
 import { getDefaultStaticProps } from '@shared/helpers/get-default-server-side-props';
 import type { DefaultSeoInfo } from '@shared/types/seo';
 import { VisitorSpaceService } from '@visitor-space/services';
 import type { VisitorSpaceInfo } from '@visitor-space/types';
 import { VisitPage } from '@visitor-space/views/VisitPage';
+import type { GetServerSidePropsResult, NextPage } from 'next';
+import type { GetServerSidePropsContext } from 'next/types';
+import type { ComponentType } from 'react';
 
 const VisitPageDutch: NextPage<DefaultSeoInfo> = (seo) => {
 	return <VisitPage {...seo} />;
@@ -19,7 +18,7 @@ export async function getServerSideProps(
 	try {
 		space = await VisitorSpaceService.getBySlug(context.query.slug as string, true);
 	} catch (err) {
-		console.error(`Failed to fetch media info by id: ${context.query.ie}`, err);
+		console.error(`Failed to fetch visitor space by slug: ${context.query.slug}`, err);
 	}
 
 	return getDefaultStaticProps(context, context.resolvedUrl, {
