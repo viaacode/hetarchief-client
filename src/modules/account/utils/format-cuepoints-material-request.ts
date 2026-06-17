49f6014fc2ad277e751a1370002904c4a51285ae
Bert Verhelst <verhelstbert@gmail.com>
2026-06-17 11:03:21 +0200

    fix(ARC-3714): use same logic as backend

--- src/modules/account/utils/format-cuepoints-material-request.ts
+++ src/modules/account/utils/format-cuepoints-material-request.ts
@@ -1,9 +1,23 @@
-import type { MaterialRequestReuseForm } from '@material-requests/types';
+import {
+	MaterialRequestDurationType,
+	type MaterialRequestReuseForm,
+} from '@material-requests/types';
 import {
 	formatDurationHoursMinutesSeconds,
 	formatDurationMinutesSeconds,
 } from '@meemoo/react-components';
+import { tText } from '@shared/helpers/translate';
 import { isNil } from 'lodash-es';
+
+// If duration is less than an hour, we format the date as mm:ss, otherwise wwe format as hh:mm:ss
+const formatTimeStamp = (value: number | undefined) => {
+	if (isNil(value)) return '';
+
+	if (value < 60 * 60) {
+		return formatDurationMinutesSeconds(value);
+	}
+	return formatDurationHoursMinutesSeconds(value);
+};
 
 export function formatCuePointsMaterialRequest(
 	reuseForm: MaterialRequestReuseForm | undefined
@@ -12,15 +26,9 @@ export function formatCuePointsMaterialRequest(
 		return '';
 	}
 
-	// If duration is less than an hour, we format the date as mm:ss, otherwise wwe format as hh:mm:ss
-	const formatTimeStamp = (value: number | undefined) => {
-		if (isNil(value)) return '';
-
-		if (value < 60 * 60) {
-			return formatDurationMinutesSeconds(value);
-		}
-		return formatDurationHoursMinutesSeconds(value);
-	};
+	if (reuseForm?.durationType === MaterialRequestDurationType.FULL) {
+		return tText('modules/account/views/account-my-application-list___volledig-bestand');
+	}
 
 	return `${formatTimeStamp(reuseForm.startTime)} - ${formatTimeStamp(reuseForm.endTime)}`;
 }
