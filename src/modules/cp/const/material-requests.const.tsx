a6cbd5637f6b5cb2f4dce1c52c70fc344d074773
Senn Patteet <senn.patteet@studiohyperdrive.be>
2026-06-18 08:41:57 +0200

    feat(ARC-3565): use info icon with tooltip when a material request has no status

--- src/modules/cp/const/material-requests.const.tsx
+++ src/modules/cp/const/material-requests.const.tsx
@@ -5,9 +5,13 @@ import {
 	type MaterialRequest,
 	MaterialRequestKeys,
 	type MaterialRequestRow,
+	MaterialRequestStatus,
 	MaterialRequestType,
 } from '@material-requests/types';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@meemoo/react-components';
 import { CopyButton } from '@shared/components/CopyButton';
+import { Icon } from '@shared/components/Icon';
+import { IconNamesLight } from '@shared/components/Icon/Icon.enums';
 import { QUERY_PARAM_KEY } from '@shared/const/query-param-keys';
 import { SortDirectionParam } from '@shared/helpers';
 import { tText } from '@shared/helpers/translate';
@@ -120,9 +124,28 @@ const getStatusColumn = (disableSort: boolean): Column<MaterialRequest> =>
 		Header: tText('modules/cp/const/material-requests___status'),
 		disableSortBy: disableSort,
 		accessor: MaterialRequestKeys.status,
-		Cell: ({ row: { original } }: MaterialRequestRow) => (
-			<MaterialRequestStatusPill status={original.status} />
-		),
+		Cell: ({ row: { original } }: MaterialRequestRow) => {
+			if (original.status === MaterialRequestStatus.NONE) {
+				return (
+					<Tooltip position="top">
+						<TooltipTrigger>
+							<span
+								className="p-material-requests__table-info-icon"
+								role="img"
+								aria-label={tText('Dit is een niet uitgebreide materiaal aanvraag zonder status')}
+							>
+								<Icon name={IconNamesLight.Info} aria-hidden />
+							</span>
+						</TooltipTrigger>
+						<TooltipContent>
+							{tText('Dit is een niet uitgebreide materiaal aanvraag zonder status')}
+						</TooltipContent>
+					</Tooltip>
+				);
+			}
+
+			return <MaterialRequestStatusPill status={original.status} />;
+		},
 	}) as Column<MaterialRequest>;
 
 const getDownloadColumn = (): Column<MaterialRequest> =>
