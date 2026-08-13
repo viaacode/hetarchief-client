5a0036c4af81e64879f73ca8baa61b8384f01662
Bert Verhelst <verhelstbert@gmail.com>
2026-08-13 11:10:45 +0200

    feat(ARC-3815): add timeline block to available blocks
    https://meemoo.atlassian.net/browse/ARC-3815

--- src/modules/admin/wrappers/admin-core-config.tsx
+++ src/modules/admin/wrappers/admin-core-config.tsx
@@ -90,6 +90,7 @@ export function getAdminCoreConfig(
 				ContentBlockType.HomepageBanner,
 				ContentBlockType.HighlightText,
 				ContentBlockType.ThemeReels,
+				ContentBlockType.Timeline,
 			],
 			defaultPageWidth: ContentPageWidth.LARGE,
 			onSaveContentPage,
