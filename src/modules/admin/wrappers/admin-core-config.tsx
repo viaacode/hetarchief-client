6ff0996eeacdf1423c7044434ea62b73a66e07a7
Bert Verhelst <verhelstbert@gmail.com>
2026-08-21 17:27:47 +0200

    fix(ARC-3877): uncomment parse search url in admin-core config
    https://meemoo.atlassian.net/browse/ARC-3877

--- src/modules/admin/wrappers/admin-core-config.tsx
+++ src/modules/admin/wrappers/admin-core-config.tsx
@@ -23,6 +23,7 @@ import { ApiService } from '@shared/services/api-service';
 import { toastService } from '@shared/services/toast-service';
 import type { Locale } from '@shared/utils/i18n';
 import { AvoCoreDatabaseType, type AvoUserCommonUser } from '@viaa/avo2-types';
+import { clientSearchUrlToApiSearchUrl } from '@visitor-space/utils/search-url-to-api-url/client-search-url-to-api-search-url';
 import Link from 'next/link';
 import type { NextRouter } from 'next/router';
 import { stringifyUrl } from 'query-string';
@@ -233,11 +234,9 @@ export function getAdminCoreConfig(
 				clear: async (_key: string) => Promise.resolve(),
 			},
 			getContentPageByLanguageAndPathEndpoint: null,
-			/*
 			search: {
 				clientSearchUrlToApiSearchUrl,
 			},
-			 */
 		},
 		database: {
 			proxyUrl: publicRuntimeConfig.PROXY_URL,
